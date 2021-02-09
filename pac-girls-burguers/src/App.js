86f695b1615f897a89ba9d8682d145cca064d56b
Kaueny Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2021-02-09 15:46:10 -0300

    docs: criação do layout forms de login e cadastro

--- pac-girls-burguers/src/App.js
+++ pac-girls-burguers/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header /> */}
+      <hr/>
+      <Login />
+      <Register/>
+      <hr/>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ pac-girls-burguers/src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Li = styled.li`
+    text-decoration: none;
+`
+const Ul = styled.ul`
+
+`
+
+const Header = () => {
+    return (
+        <nav>
+            <Ul>
+                <Li></Li>
+            </Ul>
+            
+        </nav>
+    )
+}
+
+export default Header
