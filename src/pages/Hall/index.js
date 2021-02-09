fb9ecf823d3e90ea5f0e4c70e6a6c2ffd2d3b0bf
Juliana <juliana.souzas@yahoo.com.br>
2021-02-09 16:41:52 -0300

    routes

---
+++ src/pages/Hall/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Hall(){
+    return(
+        <h1>Página salão</h1>
+    );
+};
+
+export default Hall;
---
+++ src/pages/Kitchen/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Kitchen(){
+    return(
+        <h1>Página da cozinha</h1>
+    );
+};
+
+export default Kitchen;
