49c97b445badfe977765e84460e3114d80797ec9
Tânagra Andria <design.andria@gmail.com>
2021-02-10 14:58:25 -0300

    Ajustes

---
+++ src/pages/App.css
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+
+function singnUp() {
+    return (
+      <div className="singnUp">
+        <header className="singnUp-header">
+        <form className="form">
+        <input type="text" id="email" placeholder="Digite seu e-mail"/>
+        <input type="text" id="email" placeholder="Digite seu nome"/>
+        <input type="text" id="pass" placeholder="Digite sua senha"/>
+        <button id="singnupBtn">CADASTRAR</button>
+        </form>
+        </header>
+      </div>
+    );
+  }
+  
+  export default singnUp;
