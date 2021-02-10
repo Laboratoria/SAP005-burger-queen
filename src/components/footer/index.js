a1d2cdc89176a9040de6617da52016399e28c637
elis-ctrl <elis_emb@hotmail.com>
2021-02-09 21:30:53 -0300

    devepoling Footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const footer = () => {
+  return ( 
+    <footer className= 'footer'>
+      <span>Developed by 
+        <a 
+        className='footer__link' 
+        href='https://github.com/CarolineSCosta'
+        target='_blank'
+        rel='noopener noreferrer'
+        >
+        CarolineCosta
+        </a> e
+        <a className='footer__link' 
+        href='https://github.com/Elis-ctrl'
+        target='_blank'
+        rel='noopener noreferrer'
+        >
+        ElisBrasil
+        </a>
+      </span>
+  </footer>
+  )}
+  export default footer;
