721d2bbd087c97397c6bd3230a60c10e3d077557
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-02-10 14:38:13 -0300

    organizando rotas em um arquivo unico

--- src/index.js
+++ src/index.js
@@ -2,25 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Login from './pages/Login.js';
-import Cadastro from './pages/Cadastro'
-import AnotarPedidos from './pages/AnotarPedido.js';
-import PedidosAFazer from './pages/PedidosAFazer.js';
-import PedidosProntos from './pages/PedidosProntos.js';
-
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
-  <Switch>
-    <Route path="/" component={Login} exact />
-    <Route path="/Cadastro" component={Cadastro} exact />
-    <Route path="/AnotarPedidos" component={AnotarPedidos} exact />
-    <Route path="/PedidosAFazer" component={PedidosAFazer} exact />
-    <Route path="/PedidosProntos" component={PedidosProntos} exact />
-  </Switch>
+    <Routes />
   </BrowserRouter>,
   document.getElementById('root')
 );
