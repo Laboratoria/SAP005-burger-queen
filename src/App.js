2e0adc6c1c1c84bc43ae355546498f6c445d2482
Beatriz Penalva <biapenalva@gmail.com>
2021-02-11 18:33:43 -0300

    criando o objeto usuário

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import React, { useState, useEffect } from 'react';
 const userData = {
   name: '',
   password: '',
+  confirmPassword: '',
   role: '',
   token: '',
 }
 
 const App = () => {
-  // ver como a API constrói os usuários
-
   const [user, setUser] = useState(userData);
   const [completeName, setCompleteName] = useState('');
 
@@ -20,14 +19,11 @@ const App = () => {
     setCompleteName(user.name + ' ' + user.lastName)
   }, [user.name, user.lastName])
 
-
   const handleSubmit = (event) => {
     event.preventDefault();
     alert('Usuário cadastrado com sucesso!');
     console.log(user);
-    //se senha estiver igual à confirmação da senha
     //limpar os inputs depois
-
     //no burgerlicious salvar na API
   }
 
@@ -37,38 +33,42 @@ const App = () => {
       <img src={logo} alt="logo" />
       <form>
         <p>
-        {completeName}
+          {completeName}
 
         </p>
         <label>
           Name:
-          <input type='text' value={user.name} onChange={(event) => {setUser({...user, name: event.target.value})}}/>
+          <input type='text' value={user.name} onChange={(event) => { setUser({ ...user, name: event.target.value }) }} />
         </label>
         <label>
           Last name:
-          <input type='text' value={user.lastName} onChange={(event) => {setUser({...user, lastName: event.target.value})}}/>
+          <input type='text' value={user.lastName} onChange={(event) => { setUser({ ...user, lastName: event.target.value }) }} />
         </label>
         <label>
           Email:
-          <input type='email' value={user.email} onChange={(event) => {setUser({...user, email: event.target.value})}}/>
+          <input type='email' value={user.email} onChange={(event) => { setUser({ ...user, email: event.target.value }) }} />
         </label>
         <label>
           Password:
-          <input type='password' value={user.password} onChange={(event) => {setUser({...user, password: event.target.value})}}/>
+          <input type='password' value={user.password} onChange={(event) => { setUser({ ...user, password: event.target.value }) }} />
         </label>
-        {/* <label>
+        <label>
           Confirm password:
-          <input type='password' value='' onChange={(event) => {console.log(event.target.value)}}/>
-        </label> */}
+          <input type='password' value={user.confirmPassword} onChange={(event) => { setUser({ ...user, confirmPassword: event.target.value }) }} />
+        </label>
         <label>
           Team:
-          <select onChange={(event) => {setUser({...user, role: event.target.value})}}>
+          <select onChange={(event) => { setUser({ ...user, role: event.target.value }) }}>
             <option selected disabled>Team work</option>
             <option value='Hall'>Hall</option>
             <option value='Kitchen'>Kitchen</option>
           </select>
         </label>
-        <button type='submit' value='' onClick={(event) => handleSubmit(event)}> SIGN UP </button>
+        <button type='submit' value='' onClick={(event) => { 
+          if (user.password === user.confirmPassword) { handleSubmit(event) }
+          else {
+            alert('Senha erro')
+          } }}> SIGN UP </button>
       </form>
       <Footer />
     </>
