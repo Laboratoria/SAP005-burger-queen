3870f4979e1c90637658a8dbd99f7866dd0d0b4a
karinesouza <venerandokarine@gmail.com>
2021-02-10 22:15:18 -0300

    Login funcionando

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,13 +1,45 @@
 import { Link } from 'react-router-dom';
+import React, {useState} from 'react'
 
 export const Login = () => {
-    console.log('Login');
+    
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+
+
+    const loginTeste = () => {
+        fetch('https://lab-api-bq.herokuapp.com/auth', {
+          body:`email=${email}&password=${pass}` ,
+          method: 'POST',
+          headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          
+          },
+          
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setEmail('');
+            setPass('');
+            console.log(data);
+          })
+      }
     return (
         <>
-            <input type="texto"/>
-            <input type="password"/>
-            <button id="btn-submit">Enviar</button>
+        <input type="email" placeholder="Informe seu email"
+           value={email} onChange={e=> setEmail(e.target.value)}
+           />
+            <input type="password" placeholder="Informe sua senha"
+           value={pass} onChange={e=> setPass(e.target.value)}
+           />
+            <button type='submit' onClick={(e) => {
+            e.preventDefault();
+            loginTeste();
+            }}>Logar</button>
             <Link to="/register">Cadastre-se</Link>
+            
         </>
     );
 };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -18,3 +18,6 @@ export const Register = () => {
       </>
     );
   };
+
+
+  
