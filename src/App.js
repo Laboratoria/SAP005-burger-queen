c25a524d34aabd342068cde3a2f31011503cdb46
Beatriz Penalva <biapenalva@gmail.com>
2021-02-11 10:29:52 -0300

    construção de fomrulário de cadastro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './img/logo.gif';
 
 function App() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //limpar os inputs depois
+    //no burgerlicious salvar na API
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img src={logo} alt="logo" />
+      <form>
+        <label>
+          Name:
+          <input type='text' value='' />
+        </label>
+        <label>
+          Last name:
+          <input type='text' value='' />
+        </label>
+        <label>
+          Email:
+          <input type='email' value='' />
+        </label>
+        <label>
+          Password:
+          <input type='password' value='' />
+        </label>
+        <label>
+          Confirm password:
+          <input type='password' value='' />
+        </label>
+        <label>
+          Team:
+          <select>
+            <option value="Hall">Hall</option>
+            <option value="Kitchen">Kitchen</option>
+          </select>
+        </label>
+        <button type='submit' value='' onClick={(event) => handleSubmit(event)}> SIGN UP </button>
+      </form>
+    </>
   );
 }
 
 export default App;
+
+//botão de voltar e componente footer (:
+
+// import React, { Suspense, lazy } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// const Home = lazy(() => import('./routes/Home'));
+// const About = lazy(() => import('./routes/About'));
+
+// const App = () => (
+//   <Router>
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Switch>
+//         <Route exact path="/" component={Home}/>
+//         <Route path="/about" component={About}/>
+//       </Switch>
+//     </Suspense>
+//   </Router>
+// );
