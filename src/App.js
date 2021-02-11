8ca90ae7af90b62d1aadd7bf4013412045b5a41e
Beatriz Penalva <biapenalva@gmail.com>
2021-02-11 11:13:00 -0300

    pegando os valores do input no console

--- src/App.js
+++ src/App.js
@@ -1,54 +1,63 @@
 import './App.css';
 import logo from './img/logo.gif';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  //ver como a API constrói os usuários
+  // const [users, setUsers] = useState(''); //se não tiver ninguém no lugar de data vem um array vazio
+  // const [name, setName] = useState('');
+  // const [description, setDescription] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    alert('Usuário cadastrado com sucesso!');
+    //se senha estiver igual à confirmação da senha
+    //pegar o nome, sobrenome, email, senha, time
     //limpar os inputs depois
     //no burgerlicious salvar na API
   }
 
   return (
     <>
+      <button onClick={(event) => console.log(event, 'mudar a rota')}>BACK</button>
       <img src={logo} alt="logo" />
       <form>
         <label>
           Name:
-          <input type='text' value='' />
+          <input type='text' value='' onChange={(event) => {console.log(event.target.value)}}/>
         </label>
         <label>
           Last name:
-          <input type='text' value='' />
+          <input type='text' value='' onChange={(event) => {console.log(event.target.value)}}/>
         </label>
         <label>
           Email:
-          <input type='email' value='' />
+          <input type='email' value='' onChange={(event) => {console.log(event.target.value)}}/>
         </label>
         <label>
           Password:
-          <input type='password' value='' />
+          <input type='password' value='' onChange={(event) => {console.log(event.target.value)}}/>
         </label>
         <label>
           Confirm password:
-          <input type='password' value='' />
+          <input type='password' value='' onChange={(event) => {console.log(event.target.value)}}/>
         </label>
         <label>
           Team:
-          <select>
-            <option value="Hall">Hall</option>
-            <option value="Kitchen">Kitchen</option>
+          <select onChange={(event) => {console.log(event.target.value)}}>
+            <option selected disabled>Team work</option>
+            <option value='Hall'>Hall</option>
+            <option value='Kitchen'>Kitchen</option>
           </select>
         </label>
         <button type='submit' value='' onClick={(event) => handleSubmit(event)}> SIGN UP </button>
       </form>
+      <footer>© developed by <a href="https://github.com/cbalieiro/SAP005-burger-queen">foodie! systems</a> </footer>
     </>
   );
 }
 
 export default App;
-
-//botão de voltar e componente footer (:
 
 // import React, { Suspense, lazy } from 'react';
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
