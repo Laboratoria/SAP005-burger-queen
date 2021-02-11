59d7e14183f90f1f99b47e8f4ae47a36fd4d1aaa
Lu-Pereira <luci.lv14@gmail.com>
2021-02-10 22:29:04 -0300

    api.js delete

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// Criar uma baseURL para não precisarmos digitar em cada requisição o endereço do servidor, informando apenas a rota e seus parâmetros.
-const api = axios.create({
-    baseURL: 'lab-api-bq.herokuapp.com/',
-});
-
-export default api;
