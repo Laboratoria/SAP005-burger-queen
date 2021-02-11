24df167d7e0f80d425efe1996de919469431bbf6
Julia Terin <jucterin@gmail.com>
2021-02-11 20:58:18 -0300

    JS de pág de registro finalizada.

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Login from './pages/login/index.js';
+import Register from './pages/register/index.js';
+
+
+
+const routeRender = () => {
+    const rootDiv = document.getElementById('root');
+    const routes = {
+    '/': Login,
+    '/register': Register,
+    ;
+    rootDiv.innerHTML = '';
+    rootDiv.appendChild(routes[window.location.pathname]());
+};
+
+window.addEventListener('popstate', routeRender);
+window.addEventListener('load', (e) => {
+e.preventDefault();
+verifyUserLogged((user) => {
+    if (user) {
+    onNavigate('/');
+    loadPosts();
+    } else {
+    onNavigate('/login');
+    }
+    });
+});
+routeRender();
