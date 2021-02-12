678ca43bc87f66b49c2971cd3fea144267065fbc
Alessandra <a.ale_alves@hotmail.com>
2021-02-12 01:10:49 -0300

    tela login estruturada e css ok

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,8 +10,6 @@ const Login = () => {
                 <img src="imgs/logotipo.png"alt="logo-app"/>
             </div> 
             <div className="login-right">
-                <h1>Acessa App</h1>
-
                 <div className="loginInputEmail">
                     <MdEmail/>
                     <input type="text" placeholder="Email"/>
@@ -22,15 +20,11 @@ const Login = () => {
                     <input type="text" placeholder="Senha"/>
                 </div>
 
-                <div className="loginInputRadio">
-                    <input type="radio" id="hall" name="function" value="hall"/><label>Salão</label>
-                    <input type="radio" id="hall" name="function" value="kitchen"/><label>Salão</label>
-                </div>
-
                 <button type="submit">Entrar</button>
                 <h4>Não tem uma conta?<a href="linkDeCadastro"> Cadastre-se.</a></h4>
             </div>           
-        </div>    
+        </div>
+            
     )
 }
 
