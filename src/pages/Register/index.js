54f851d22fb64f0bf2dd378bd12b76bd86da2c90
Julia Terin <jucterin@gmail.com>
2021-02-12 20:36:52 -0300

    inicio da rota

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,86 +1,100 @@
-import React { useState } from 'react'
+import { React, useState } from 'react';
 import './register.css';
 
 import { MdPerson, MdEmail, MdLock, MdVisibility, MdVisibilityOff } from 'react-icons/md';
-import { useState } from 'react';
 
-function Register() {
-    const  [email, setEmail] = useState ("")
-    const  [password, setPassword] = useState ("")
-    const  [passwordConfirmation, setPasswordConfirmatio] = useState ("")
-    const  [show, setShow] = useState (false)
+const Register = () => {
+    const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ passwordConfirmation, setPasswordConfirmation ] = useState('');
+	const [ show, setShow ] = useState(false);
+	const [ role, setRole ] = useState('');
 
-const handleClick = (e) => {
-    e.preventDefault()
-    setShow(!show)
-}
+	function registre(event) {
+		event.preventDefault();
+		fetch('https://lab-api-bq.herokuapp.com/users/', {
+			method: 'POST',
+			headers: {
+				accept: 'application/json',
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: `email=${email}&password=${password}&role=${role}&restaurant=LaBurger&name=${name}`
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json));
+	}
 
-    }
-    return (
-        <div className="register-page">
-        <div className="left">
-        <img src="../img/logo.jpg" width="175px" height="175px" alt="Logo da marca LaBurguer"/>
-        </div> 
-        <div className="right">
-            <h1>Registre-se</h1>
+	const handleClick = (e) => {
+		e.preventDefault();
+		setShow(!show);
+	};
 
-            <div className="input-in-line">
-            <MdPerson/>
-                <input type="text" placeholder="Nome"/>
-            </div>
+	return (
+		<div className="register-page">
+			<div className="left">
+				<img src="../img/logo.jpg" width="175px" height="175px" alt="Logo da marca LaBurguer" />
+			</div>
+			<div className="right">
+				<h1>Registre-se</h1>
 
-            <div className="input-in-line">
-            <MdEmail/>
-                <input type="email"
-                placeholder="Email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}/>
-            </div>
+                <div className="input-in-line">
+					<MdPerson />
+					<input type="name" placeholder="Nome" value={name} onChange={(e) => setName(e.target.value)} />
+				</div>
 
-            <div className="input-in-line">
-            <MdLock/>
-                <input type={show ? "text" : "password"}
-                placeholder="Senha"
-                value={password}
-                onChange={e => setPassword(e.target.value)}/>
-                <div className="registe-eye">
-                    { show ? (
-                    <MDFVisibility
-                    size={20}
-                    onClick={handleClick}
-                    />   
-                    ) : (
-                    <MdVisibilityOff
-                    size={20}
-                    onClick={handleClick}F/>    
-                    )}
-                </div>
-            </div>
+                <div className="input-in-line">
+					<MdEmail />
+					<input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+				</div>
 
-            <div className="input-in-line">
-            <MdLock/>
-                <input type="text" 
-                placeholder="Confirme sua senha"
-                value={passwordConfirmation}
-                onChange={e => setPasswordConfirmatio(e.target.value)}/>
-                <div className="registe-eye">
-                { show ? (
-                    <MDFVisibility
-                        size={20}
-                        onClick={handleClick}
-                        />   
-                    ) : (
-                    <MdVisibilityOff
-                    size={20}
-                    onClick={handleClick}F/>    
-                    )}
-                </div>
-            </div>
+				<div className="input-in-line">
+					<MdLock />
+					<input
+						type={show ? 'text' : 'password'}
+						placeholder="Senha"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<div className="registe-eye">
+						{show ? (
+							<MdVisibility size={20} onClick={handleClick} />
+						) : (
+							<MdVisibilityOff size={20} onClick={handleClick} />
+						)}
+					</div>
+				</div>
 
-            <button type="submit">Registrar</button>
-            
-        </div>           
-    </div>    
-)
+				<div className="input-in-line">
+					<MdLock />
+					<input
+						type="text"
+						placeholder="Confirme sua senha"
+						value={passwordConfirmation}
+						onChange={(e) => setPasswordConfirmation(e.target.value)}
+					/>
+					<div className="registe-eye">
+						{show ? (
+							<MdVisibility size={20} onClick={handleClick} />
+						) : (
+							<MdVisibilityOff size={20} onClick={handleClick} />
+						)}
+					</div>
+				</div>
+			</div>
 
+			<select value={role} onChange={(event) => setRole(event.target.value)}>
+				<option>Função</option>
+				<option value="Cozinha">Cozinha</option>
+				<option value="Salão">Salão</option>
+			</select>
+
+			<div>
+				<button type="submit" onClick={registre}>
+					Registrar
+				</button>
+			</div>
+		</div>
+	);
+};
 export default Register;
