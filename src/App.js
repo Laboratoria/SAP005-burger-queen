66fd7ed26d6aaf3b9bbf7a8593b1ed790c04126f
Mairã Soares <71467930+MairaSoares@users.noreply.github.com>
2021-02-12 00:29:08 -0300

    Lógica do registro da aplicação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function register(email, password, role, restaurant, nome){
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("email", `${email}`);
+    urlencoded.append("password", `${password}`);
+    urlencoded.append("role", `${role}`);
+    urlencoded.append("restaurant", `${restaurant}`);
+    urlencoded.append("name", `${nome}`);
+
+  var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: 'follow'
+  };
+
+  fetch("https://lab-api-bq.herokuapp.com/users", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  }
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+  const [restaurant, setRestaurant] = useState('');
+  const [nome, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    register (email, password, role, restaurant, nome)
+  }
+
+  return(
+    <form>
+        <label>
+          Email:
+          <input type="text" value={email} onChange={(event) => setEmail(event.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="text" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </label>
+        <label>
+          Role:
+          <input type="text" value={role} onChange={(event) => setRole(event.target.value)} />
+        </label>
+        <label>
+          Restaurant:
+          <input type="text" value={restaurant} onChange={(event) => setRestaurant(event.target.value)} />
+        </label>
+        <label>
+          Name:
+          <input type="text" value={nome} onChange={(event) => setName(event.target.value)} />
+        </label>
+        <input type="submit" value="Enviar" onClick={(event) => handleSubmit(event)}/>
+      </form>
+  )
+  
 }
 
 export default App;
