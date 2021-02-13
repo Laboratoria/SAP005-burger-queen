05a186884ad5c6c9a698f7e924745e5bd3f10abf
RoSapia <RoSapia>
2021-02-13 14:50:43 -0300

    criando rotas no react

---
+++ src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Register = () => (
+    <div>
+        <header>
+            <h1>Página Registro</h1>
+        </header>
+        <p>
+            <Link to="/">Home</Link>
+        </p>
+    </div>
+);
