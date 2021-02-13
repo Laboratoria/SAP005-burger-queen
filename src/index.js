b321b36b5ca893186961a7eb9c5627c54319552c
Alessandra <a.ale_alves@hotmail.com>
2021-02-13 17:10:08 -0300

    atualiz. de rotas entre login e cadastro

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-/* eslint-disable react/jsx-no-undef */
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import  Login from './pages/Login/index.js';
+import  Register from './pages/Register/index.js';
 
-import Login from "./pages/Login/index";
-import Register from "./pages/Register/index";
-import { BrowserRouter } from 'react-router-dom'
-
-import { Login } from "./pages/Login/index";
-import { Register } from "./pages/Register/index";
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Login} exact />
-      <Route path="/register" component={Register} />
+      <Route path="/register" component={Register} exact/>
       <Route component={() => <div>Page 404!"</div>} />
     </Switch>
   </BrowserRouter>,
