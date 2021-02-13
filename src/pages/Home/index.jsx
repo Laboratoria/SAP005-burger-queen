b273f347af884b89cc86e54a64d5138dd1d4ceb3
cbalieiro <bmoliveira.camila@gmail.com>
2021-02-13 11:51:15 -0300

    Fix de algumas estruturas ES6, reactJS e conclusão de codigo para acesso.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>ESSA É A HOME</h1>
-      <p>
-        <Link to="/Login"> VOLTE PARA O LOGIN </Link>
-      </p>
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <h1>ESSA É A HOME</h1>
+    <p>
+      <Link to="/Login"> VOLTE PARA O LOGIN </Link>
+    </p>
+  </div>
+);
 
 export default Home;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,35 +1,85 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../components/logo";
 import Container from "../../components/main";
 import Footer from "../../components/footer.js";
 
+const userData = {
+  name: '',
+  email: '',
+  role: '',
+}
+
+const loginPage = (email, password) => {
+  fetch('https://lab-api-bq.herokuapp.com/auth', {
+    method: 'POST',
+    headers: { 'accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `email=${email}&password=${password}`,
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      alert('Acessou');
+    });
+};
+
 const Login = () => {
+  const [user, setUser] = useState(userData);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    loginPage(user.email, user.password);
+  }
+
   return <>
     <Container>
-       <div className="inputs-container">
-       <Logo />
+      <div className="inputs-container">
+        <Logo />
         <form>
-          <label for="login">Login:
-            <input type="text" placeholder="email@email.com" />
+          <label htmlFor="login">
+            Login:
+              <input
+              type="text"
+              value={user.email}
+              onChange={(event) => {
+                setUser({ ...user, email: event.target.value });
+              }}
+              placeholder="email@email.com"
+            />
           </label>
-          <label for="password">Password:
-            <input type="password" placeholder="Password" />
+          <label htmlFor="password">
+            Password:
+              <input
+              type="password"
+              value={user.password}
+              onChange={(event) => {
+                setUser({ ...user, password: event.target.value });
+              }}
+              placeholder="Password"
+            />
           </label>
-          <label for="team">Team:
-            <select onChange={(event) => { console.log(event); }} defaultValue='Team work'>
+          <label htmlFor="team">
+            Team:
+              <select
+              onChange={(event) => {
+                setUser({ ...user, role: event.target.value });
+              }}
+              defaultValue="Team work"
+            >
               <option disabled>Team work</option>
-              <option value='Hall'>Hall</option>
-              <option value='Kitchen'>Kitchen</option>
+              <option value="Hall">Hall</option>
+              <option value="Kitchen">Kitchen</option>
             </select>
           </label>
-          <button type='submit' value='' onClick={(event) => {console.log(event);}}> SIGN UP </button>
-            <p>Do not have an account?<span><Link to="/Register">Register</Link></span></p>
+          <button type='submit' value='' onClick={(event) => { handleSubmit(event) }}> SIGN UP </button>
+          <p>Do not have an account?<span><Link to="/Register">Register</Link></span></p>
         </form>
       </div>
     </Container>
     <Footer />
   </>;
-};
-export default Login;
+}
+
+export default Login
