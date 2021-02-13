03d1b13e7844d62df5e54edfb8fe197e12fdd0d7
cbalieiro <bmoliveira.camila@gmail.com>
2021-02-13 12:56:04 -0300

    Criando Objeto para realizarr testes

---
+++ src/components/Objetos/requestOptions.js
@@ -0,0 +1,15 @@
+const requestOptions = {
+  post(props) {
+    const request = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `${props}`,
+    };
+    return request;
+  },
+};
+
+export default requestOptions;
