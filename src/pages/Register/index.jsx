32bf752de41aa94e6757dca879783ca7f9ccfe71
Beatriz Penalva <biapenalva@gmail.com>
2021-02-13 13:48:07 -0300

    classes para estilizacao da tela e componente logo

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../img/logo.gif";
 import Footer from "../../components/footer.js";
+import Logo from "../../components/logo";
 
 const userData = {
   name: '',
@@ -41,48 +41,47 @@ const Register = () => {
   return (
     <>
       <p><Link to="/Login">BACK</Link></p>
+      <div className="inputs-container">
+        <Logo />
+        <form>
+          <label>
+            Name:
+          <input type='text' value={user.name} onChange={(event) => { setUser({ ...user, name: event.target.value }) }} placeholder="Name"/>
+          </label>
+          <label>
+            Last name:
+          <input type='text' value={user.lastName} onChange={(event) => { setUser({ ...user, lastName: event.target.value }) }} placeholder="Last name"/>
+          </label>
+          <label>
+            Email:
+          <input type='email' value={user.email} onChange={(event) => { setUser({ ...user, email: event.target.value }) }} placeholder="email@email.com"/>
+          </label>
+          <label>
+            Password:
+          <input type='password' value={user.password} onChange={(event) => { setUser({ ...user, password: event.target.value }) }} placeholder="Password"/>
+          </label>
+          <label>
+            Confirm password:
+          <input type='password' value={user.confirmPassword} onChange={(event) => { setUser({ ...user, confirmPassword: event.target.value }) }} placeholder="Password"/>
+          </label>
 
-      <img src={logo} alt="logo" />
+          <label>
+            Team:
+          <select className="select-style" onChange={(event) => { setUser({ ...user, role: event.target.value }) }} defaultValue='Team work'>
+              <option disabled>Team work</option>
+              <option value='Hall'>Hall</option>
+              <option value='Kitchen'>Kitchen</option>
+            </select>
+          </label>
 
-      <form>
-        <label>
-          Name:
-          <input type='text' value={user.name} onChange={(event) => { setUser({ ...user, name: event.target.value }) }} />
-        </label>
-        <label>
-          Last name:
-          <input type='text' value={user.lastName} onChange={(event) => { setUser({ ...user, lastName: event.target.value }) }} />
-        </label>
-        <label>
-          Email:
-          <input type='email' value={user.email} onChange={(event) => { setUser({ ...user, email: event.target.value }) }} />
-        </label>
-        <label>
-          Password:
-          <input type='password' value={user.password} onChange={(event) => { setUser({ ...user, password: event.target.value }) }} />
-        </label>
-        <label>
-          Confirm password:
-          <input type='password' value={user.confirmPassword} onChange={(event) => { setUser({ ...user, confirmPassword: event.target.value }) }} />
-        </label>
-
-        <label>
-          Team:
-          <select onChange={(event) => { setUser({ ...user, role: event.target.value }) }} defaultValue='Team work'>
-            <option disabled>Team work</option>
-            <option value='Hall'>Hall</option>
-            <option value='Kitchen'>Kitchen</option>
-          </select>
-        </label>
-
-        <button type='submit' value='' onClick={(event) => {
-          if (user.password === user.confirmPassword) { handleSubmit(event) }
-          else {
-            alert('Senha erro')
-          }
-        }}> SIGN UP </button>
-      </form>
-
+          <button type='submit' value='' onClick={(event) => {
+            if (user.password === user.confirmPassword) { handleSubmit(event) }
+            else {
+              alert('Senha erro')
+            }
+          }}> SIGN UP </button>
+        </form>
+      </div>
       <Footer />
     </>
   );
