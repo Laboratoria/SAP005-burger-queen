82d0b2b2dcb79a9e247bcf2a2854a7f0716a888d
crismantovani <crismantovani87@gmail.com>
2021-02-14 16:59:54 -0300

    Criação do componente Footer.

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -3,6 +3,7 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import Footer from '../../components/Footer';
 
 export default function Login() {
   const apiURL = 'https://lab-api-bq.herokuapp.com';
@@ -75,6 +76,8 @@ export default function Login() {
           buttonText='Cadastre-se'
         />
       </Link>
+
+      <Footer />
     </Fragment>
   );
 }
