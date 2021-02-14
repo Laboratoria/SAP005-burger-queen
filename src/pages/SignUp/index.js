747f672c2c2e0880b46e61a72fe2dda238e8a772
alencartha <alencar_tha@hotmail.com>
2021-02-14 17:21:34 -0300

    cadastro, login e useHistory nos retornos das funções

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import React, { useState} from 'react';
 import '../../Styles/Cadastro.css';
 import Header from '../../components/HeaderLogin/index.js';
 import Footer from '../../components/Footer/index.js';
 
 
+
 function Cadastro() {
+  
+  const history = useHistory();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('');
+ 
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+ 
+    
+    const options = {
+      method: 'POST',
+      headers: { accept: 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded' 
+              },
+      body: `email=${email}&password=${password}&role=${role}&restaurant=BurgerBeef&name=${name}`
+    };
+
+    fetch('https://lab-api-bq.herokuapp.com/users', options)
+    .then(response=>{response.json()
+        .then(data=>{
+          history.push('/');
+        })})
+    .catch(e => console.log("Deu erro" + e.message))
+
+}
+
   return (
     <div className="page-cadastro">
 
@@ -13,40 +45,46 @@ function Cadastro() {
       <form className="form-cadastro">
         <label>
           Nome:
-          <input type="text" name='nome' className='input-text'/>
+          <input type="text" name='nome' className='input-text' value={name} onChange={(event) => setName(event.target.value)}/>
         </label>
 
         <label>
           E-mail:
-          <input type="text" name='email' className='input-text' />
+          <input type="text" name='email' className='input-text' value={email}  onChange={(event) => setEmail(event.target.value)}/>
         </label>
 
         <label>
           Senha:
-          <input type="password" name='senha' className='input-text' />
+          <input type="password" name='senha' className='input-text' value={password} onChange={(event) => setPassword(event.target.value)} />
         </label>
 
-        <section className='option-setor'>
+        <label>
+          Role:
+          <input type="text" name='role' className='input-text' value={role} onChange={(event) => setRole(event.target.value)} />
+        </label>
+
+        {/* <section className='option-setor'>
         <p className='setor'>Setor:</p>
           <label>
-            <input type="radio" value="cozinha" name="role" className='input-radio'/>
+            <input type="radio" value="cozinha" name="role" className='input-radio' value={role} onChange={(event) => setRole(event.target.value)}/>
             Cozinha
           </label>
 
           <label>
-            <input type="radio" value="salão" name="role" className='input-radio' />
+            <input type="radio" value="salão" name="role" className='input-radio' value={role} onChange={(event) => setRole(event.target.value)}/>
             Salão
           </label>
-        </section>
-
+        </section> */}
+{/* 
         <label>
           Restaurante:
           <input type="text" name='nomeRestaurante' className='input-text'/>
-        </label>
+        </label> */}
 
-        <button type='submit' value='enviar' className='btn-cadastrar'><Link to='/'>CADASTRAR</Link></button>
+        <button  value='enviar' className='btn-cadastrar' onClick={handleSignUp}>CADASTRAR</button>
       </form>
 
+        <button><Link to='/'>Login</Link></button>
       <Footer/>
       
     </div>
