926a1806c076c1bd6eb89fe7cc1d6f70e0008ec4
RoSapia <RoSapia>
2021-02-14 09:45:56 -0300

    adicionando radio ao form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,11 +2,24 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Register = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [senha, setPassword] = useState('');
+  const [formValues, setFormValues] = useState({});
 
-    const handleName = (event) => {
+  const handleInputChange = (event) => {
+    const { name, value, password, type, checked } = event.target;
+    setFormValues({ ...formValues, [name]: value});
+  }
+    /*const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setPassword] = useState('');*/
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log('*** handleSubmit', data);
+  };
+
+    /*const handleName = (event) => {
         setName(event.target.value);
     }
 
@@ -19,31 +32,37 @@ export const Register = () => {
       setPassword(event.target.value);
       };  
 
+    const handleHole = (event) => {
+        setPassword(event.target.value);
+        };    
+
     const handleSubmit = (event) => {
         event.preventDefault();
         setEmail();
         setPassword();
 
-    }
+    }*/
 return(
     <div>
         <header>
             <h1>Página de Registro</h1>
         </header>
-        <form>
-          <input type='text' placeholder='Nome' value={name} onChange={handleName} /> 
-          <input type='text' placeholder='E-mail' value={email} onChange={handleEmail} />
-          <input type='password' placeholder='Senha' value={senha} onChange={handlePassword} />
-          <p>Local de Trabalho</p>
-          <p>
-          <input type='radio' name='role' value='kitchen'/>
-          <label for='kitchen'>Cozinha</label>
-          <input type='radio' name='role' value='lounge'/>
-          <label for='lounge'>Salão</label>
-          </p>
-          <button type='submit' onClick={(event) => handleSubmit(event)}>Enviar</button>
-          </form>
-        <p>
+        <form onSubmit={handleSubmit}>
+          <input type='text' name='name' placeholder='Nome' onChange={handleInputChange} value={formValues.name || ''} /> 
+          <input type='text' name='email' placeholder='E-mail' onChange={handleInputChange} value={formValues.email || ''} />
+          <input type='password' name='password' placeholder='Senha' onChange={handleInputChange} value={formValues.password || ''} />
+          <div>
+            Local de Trabalho
+            <label>
+                <input type='radio' name='role' value='kitchen' onChange={handleInputChange} checked={formValues.role === 'kitchen'} />Cozinha
+            </label>
+            <label>
+                <input type='radio' name='role' value='lounge' onChange={handleInputChange} checked={formValues.role === 'lounge'} />Salão
+            </label>
+          </div>
+          <button type='submit'>Enviar</button>
+        </form>
+        <p>            
             <Link to="/">Home</Link>
         </p>
     </div>
