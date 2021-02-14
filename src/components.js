8aa868b27c52983d2f07f7a03e01fb80ac776ae4
JulianaAmoriN <juamorim.sn@gmail.com>
2021-02-14 18:04:07 -0300

    css do Login completo

---
+++ src/components.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: 0,
+    textAlign: 'center',
+    
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: theme.palette.text.primary,
+    color: '#fafafa'
+  },
+  logo: {
+    maxWidth: 200,
+  },
+  
+}));
+
+export function Copyright() {
+  return (
+    <Box mt={5}>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography> 
+    </Box>
+  );
+}
+
+export function SingIn() {
+  return (
+    <Typography color="textPrimary" component="h1" variant="h5">
+    Ratatouille Burguer
+    </Typography>
+  )
+}
+
+  
