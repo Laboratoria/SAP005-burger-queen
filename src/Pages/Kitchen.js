be9b64c1d5aced5e3dd0763b98972fd8b69bd4ad
letbrasil <le.brasileiro@gmail.com>
2021-02-13 21:55:37 -0300

    redireciona tela após login/cadastro

---
+++ src/Pages/Kitchen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Kitchen() {
+	return (
+		<div>
+			<h1>Cozinha</h1>
+		</div>
+	)
+}
+
+export default Kitchen;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Pages/Register';
+// import App from './Pages/Register';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Saloon from './Pages/Saloon';
+import Kitchen from './Pages/Kitchen';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Login} exact />
+        <Route path='/register' component={Register} exact />
+        <Route path='/saloon' component={Saloon} exact />
+        <Route path='/kitchen' component={Kitchen} exact />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
