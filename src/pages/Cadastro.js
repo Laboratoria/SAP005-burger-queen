cee5c5bd036b068e295a0b4b63bc547c79fe51c3
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-02-14 19:20:03 -0300

    pagina cadastro ok

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import FormCadastro from '../components/FormCadastro.js';
 import Logo from '../assets/Logo_Burger-Beef .png';
 import './Cadastro.css';
 
@@ -8,42 +8,7 @@ function Cadastro() {
       <header className="header-cadastro">
         <img className="logo-cadstro" title="logo" src={Logo} alt='logo' />
       </header>
-      <form className="form-cadastro">
-        <label>
-          Nome:
-          <input type="text" name='nome' className='input-text'/>
-        </label>
-
-        <label>
-          E-mail:
-          <input type="text" name='email' className='input-text' />
-        </label>
-
-        <label>
-          Senha:
-          <input type="password" name='senha' className='input-text' />
-        </label>
-
-        <section className='option-setor'>
-        <p className='setor'>Setor:</p>
-          <label>
-            <input type="radio" value="cozinha" name="role" className='input-radio'/>
-            Cozinha
-          </label>
-
-          <label>
-            <input type="radio" value="salão" name="role" className='input-radio' />
-            Salão
-          </label>
-        </section>
-
-        <label>
-          Restaurante:
-          <input type="text" name='nomeRestaurante' className='input-text'/>
-        </label>
-
-        <button type='submit' value='enviar' className='btn-cadastrar'><Link to='/'>CADASTRAR</Link></button>
-      </form>
+      <FormCadastro />
     </div>
   );
 }
