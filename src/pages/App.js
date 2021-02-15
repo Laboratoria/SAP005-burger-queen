c146f70fafc9b18fe3ab1a17beee5d596db41b35
LuanaGss <luana_gss@hotmail.com>
2021-02-15 13:12:27 -0300

    atualizar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src= {logo} alt="" className="logo"/>
-
-        <form className="form">
-        <input type="text" id="email" placeholder="Digite seu e-mail"/>
-        <input type="text" id="pass" placeholder="Digite sua senha"/>
-        <button id="login">ENTRAR</button>
-        </form>
-        <Link to="/singnUp" className="register">Cadastre-se aqui </Link>
+        <img src={logo} alt="" className="logo" />
+        <div className="registerLogin">
+          <form className="form">
+            <input type="text" id="email" placeholder="Digite seu e-mail" />
+            <input type="text" id="pass" placeholder="Digite sua senha" />
+            <button id="login">ENTRAR</button>
+          </form>
+          <Link to="/singnUp" className="register">Não possuí conta? Cadastre-se. </Link>
+        </div>
       </header>
     </div>
   );
--- src/pages/singnUp.js
+++ src/pages/singnUp.js
@@ -6,23 +6,31 @@ import hand from './hand.png';
 
 
 function singnUp() {
-    return (
-      <div className="singnUp">
-        <header className="singnUp-header">
-        <img src= {logo} alt="" className="logo"/>
-        <p className="selector">
-        <img src= {panela} alt="" className="logop"/>
-        <img src= {hand} alt="" className="logoh"/>
-        </p>
-        <form className="form">
-        <input type="text" id="email" placeholder="Digite seu e-mail"/>
-        <input type="text" id="email" placeholder="Digite seu nome"/>
-        <input type="text" id="pass" placeholder="Digite sua senha"/>
-        <button id="singnupBtn">CADASTRAR</button>
-        </form>
-        </header>
-      </div>
-    );
-  }
-  
-  export default singnUp;
+  return (
+    <div className="singnUp">
+      <header className="singnUp-header">
+        <img src={logo} alt="" className="logo" />
+        <div className="divRegister">
+          <div className="selector">
+            <label>
+              <img src={panela} alt="" className="logop" />
+              <input type="radio" id="selectFunction" name="kitchen" value="yes" checked="" />
+            </label>
+            <label>
+              <img src={hand} alt="" className="logoh" />
+              <input type="radio" id="selectFunction" name="hall" value="no" />
+            </label>
+          </div>
+          <form className="form">
+            <input type="text" id="email" placeholder="Digite seu e-mail" />
+            <input type="text" id="email" placeholder="Digite seu nome" />
+            <input type="text" id="pass" placeholder="Digite sua senha" />
+            <button id="singnupBtn">CADASTRAR</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default singnUp;
