b869b441d233c73192183b5b5a687534df158457
LuanaGss <luana_gss@hotmail.com>
2021-02-15 13:45:35 -0300

    atualizar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 import React from "react";
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="" className="logo" />
-        <div className="registerLogin">
-          <form className="form">
-            <input type="text" id="email" placeholder="Digite seu e-mail" />
-            <input type="text" id="pass" placeholder="Digite sua senha" />
-            <button id="login">ENTRAR</button>
-          </form>
-          <Link to="/singnUp" className="register">Não possuí conta? Cadastre-se. </Link>
-        </div>
+        <img src= {logo} alt="" className="logo"/>
+
+        <form className="form">
+        <input type="text" id="email" placeholder="Digite seu e-mail"/>
+        <input type="text" id="pass" placeholder="Digite sua senha"/>
+        <button id="login">ENTRAR</button>
+        </form>
+        <Link to="/singnUp" className="register">Cadastre-se aqui </Link>
       </header>
     </div>
   );
