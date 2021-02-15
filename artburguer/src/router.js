2ba8e35bd92d7a38c92886afa88f3c1d939959d1
Rebeca Canesin <rebecafguilherme@gmail.com>
2021-02-15 16:56:11 -0300

    rotas privadas iniciadas

--- artburguer/src/router.js
+++ artburguer/src/router.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useContext, createContext, useState } from "react";
 import Login from './pages/login.js';
 import Register from './pages/register.js'; 
 import Orders from './pages/orders.js';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Router, Redirect,
+    useHistory,
+    useLocation } from 'react-router-dom';
 
 const Routes = () =>{
     return(
-        <Switch>
-            <Route path='/' exact component={Login} />
-            <Route path='/register' exact component={Register} />
-            <PrivateRoute path='/orders' exact component={Orders} >
-                <ProtectedPage/>
-                
-            </PrivateRoute>
-        </Switch>
+       
+            <Router>
+                <Switch>
+                    <Route path='/' exact component={Login} />
+                    <Route path='/register' exact component={Register} />
+                    <Route path='/orders' exact component={Orders} />
+                        
+                    
+                </Switch>
+
+            </Router>
+       
+        
     )
 };
 
