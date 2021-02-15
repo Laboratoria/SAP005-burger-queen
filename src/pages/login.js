b4a18c909f2cd2379d733bcb4ae1427f6944fd34
KarinaFS <karina_fsantos@outlook.com>
2021-02-15 16:32:41 -0300

    Entrar na tela correta para cada usuário

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import './login.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function LoginBtn(e) {
-        e.preventDefault();
+    const routes = useHistory();
 
+    const loungeRoute = () => {
+        routes.push('/lounge')
+    }
 
+    const kitchenRoute = () => {
+        routes.push('/kitchen')
+    }
+
+    function LoginBtn(event) {
+        event.preventDefault();
 
     fetch('https://lab-api-bq.herokuapp.com/auth', {
         method: 'POST',
@@ -24,6 +32,12 @@ export const Login = () => {
         .then(result => result.json())
         .then(result => {
             console.log(result);
+            if(result.role === "lounge") {
+                loungeRoute();
+            }
+            if(result.role === "kitchen") {
+                kitchenRoute();
+            }
 
         });
     }    
@@ -37,7 +51,7 @@ export const Login = () => {
             <form>
                 <input type="text" name="email" placeholder="E-mail" id="input-login" value={email} onChange={(event) => setEmail(event.target.value)} />
                 <input type="password" name="password" placeholder="Senha" id="input-password" value={password} onChange={(event) => setPassword(event.target.value)} />
-                <button type="submit" id="login" onClick={LoginBtn}>Entrar</button>
+                <button type="submit" id="login-btn" onClick={LoginBtn}>Entrar</button>
             </form>
             <p>
                 Não possui cadastro?
