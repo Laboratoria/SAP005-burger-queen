94f94c243d9d287878ec955489368c662a81b7ed
caroline costa <carolmarcionilia@gmail.com>
2021-02-15 14:46:40 -0300

    fix navbar link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,8 +11,8 @@ export default function Navbar() {
     <Fragment>
       <navbar className='responsive-wrap-navbar'>
         <p className='label-nav'><FaUserCircle className='icon-nav' /><br />Carol Costa</p>
-        <Link className='link' to='/signup'><p className='label-nav'><FaRegClock className='icon-nav' /><br />Status Pedidos</p></Link>
-        <Link className='link' to='/signup'><p className='label-nav'><GiKnifeFork className='icon-nav' /><br />Novo Pedido</p></Link>
+        <Link className='link' to='/status-order'><p className='label-nav'><FaRegClock className='icon-nav' /><br />Status Pedidos</p></Link>
+        <Link className='link' to='/new-order'><p className='label-nav'><GiKnifeFork className='icon-nav' /><br />Novo Pedido</p></Link>
         <Link className='link' to='/'><p className='label-nav'><FaDoorOpen className='icon-nav' /><br />Sair</p></Link>
       </navbar>
     </Fragment>
