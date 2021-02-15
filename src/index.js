8c819c2dcd109adc669c634977529c21262e57a9
Sara Viana <saravianac@outlook.com>
2021-02-15 11:01:18 -0300

    CODE: Rotas finalizadas, falta personalizar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import Cadastro from './pages/Cadastro';
 import App from './App';
 import Login from './pages/Login';
-import Confirmado from './pages/Confirmado';
 import Salao from './pages/Salao';
 import Cozinha from './pages/Cozinha';
 import reportWebVitals from './reportWebVitals';
@@ -16,7 +15,6 @@ ReactDOM.render(
       <Route path = "/" component={App}  exact/>
       <Route path = "/cadastro" component={Cadastro} exact/>
       <Route path = "/login" component={Login} exact/>
-      <Route path = "/confirmado" component={Confirmado} exact/>
       <Route path = "/salao" component={Salao} exact/>
       <Route path = "/cozinha" component={Cozinha} exact/>
     </Switch>  
@@ -24,7 +22,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -5,11 +5,11 @@ import './Cadastro.css';
 function Cadastro() {
   const history = useHistory()
 
-  const routerConfirm = ()=>{
-    history.push('/confirmado')
+  const routerConfirm = () => {
+    history.push('/login')
   }
 
-  const [name, setName] = useState('');
+   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [role, setRole] = useState('');
@@ -21,13 +21,13 @@ function Cadastro() {
       <h1 className="CadTitle">Cadastro</h1>
       <form className="FormCadastro">
         <label className="cadLabel" htmlFor="cadInputName"></label>
-        <input type="text" placeholder="Nome do Funcionário" className="cadInput" value={name} onChange={(event) => setName(event.target.value)} />
+        <input requered type="text" placeholder="Nome do Funcionário" className="cadInput" value={name} onChange={(event) => setName(event.target.value)} />
 
         <label className="cadLabel" htmlFor="cadInputEmail"></label>
-        <input type="text" placeholder="E-mail" className="cadInput" value={email} onChange={(event) => setEmail(event.target.value)} />
+        <input requered type="text" placeholder="E-mail" className="cadInput" value={email} onChange={(event) => setEmail(event.target.value)} />
 
         <label className="cadLabel" htmlFor="cadInputPassword"></label>
-        <input type="password" placeholder="Senha" className="cadInput" value={password} onChange={(event) => setPassword(event.target.value)} />
+        <input requered type="password" placeholder="Senha" className="cadInput" value={password} onChange={(event) => setPassword(event.target.value)} />
 
         <label className="cadLabel" htmlFor="cadInputRole">Cargo:</label>
         <select name="ordenar" className="cadInput cardSelect" value={role} onChange={(event) => setRole(event.target.value)}>
