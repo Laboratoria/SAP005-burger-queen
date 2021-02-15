205de2327cf8c2ceb32d85c079d008e80a3ca40b
KarinaFS <karina_fsantos@outlook.com>
2021-02-15 17:28:20 -0300

    apagando pastas desnecessárias

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
@@ -9,7 +7,6 @@ import { Routes } from './routes';
 ReactDOM.render(
   <BrowserRouter>
     <Routes />
-    {/* <App /> */}
   </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import './login.css';
 import { Link, useHistory } from 'react-router-dom';
+import './login.css';
 
 export const Login = () => {
 
