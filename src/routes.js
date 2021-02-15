4be2c233746cbbee859a0807a366ac911fe8f4b8
RoSapia <RoSapia>
2021-02-15 17:23:41 -0300

    retirando arquivos desnecessarios

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
-import NavBar from './ui/NavBar.js';
 
 import { Switch, Route } from 'react-router-dom';
 
