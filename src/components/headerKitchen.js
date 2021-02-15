4a559b8c0104a11171d285fd1ccb461c5296d49f
cbalieiro <bmoliveira.camila@gmail.com>
2021-02-15 10:17:18 -0300

    criado componente headerkiten para testes da pagina cozinha

---
+++ src/components/headerKitchen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../img/logo.gif';
+
+const HeaderKitchen = () => <div>
+<div>Estou</div>
+<img src={logo} alt="logo" height="165px" width="164px" />;
+<div>testando</div>
+</div>;
+
+export default HeaderKitchen;
--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HeaderKitchen from '../../components/headerKitchen'
 
 const Kitchen = () => (
   <div>
+    <HeaderKitchen />
     <h1>Aqui só tem garçons</h1>
     <p>
       <Link to="/Hall"> VOLTE PARA O LOGIN </Link>
