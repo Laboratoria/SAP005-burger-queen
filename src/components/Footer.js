42c9f9201a3ac06c76a56bc63bc799f272079079
Alessandra <a.ale_alves@hotmail.com>
2021-02-16 01:23:33 -0300

    ajustes/ registro

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Footer.css';
+import { GoOctoface } from "react-icons/go";
 
-const Footer = (props) => {
-  return (
-    <footer className={props}>Desenvolvido por Alessandra Alves e Julia Terin</footer>
-  );
-}
-
+const Footer = () => (
+  <footer>
+    <p className="github">
+      <GoOctoface/> Desenvolvido por <a href="https://github.com/ale-alves"><strong> Alessandra Alves </strong></a> e
+       <a href="https://github.com/juliaterin"><strong> Julia Terin</strong></a>
+    </p>
+  </footer>
+);
 export default Footer;
