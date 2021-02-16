7462343e52c11ae1400a18911be8cc96c117d87e
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2021-02-16 16:32:21 -0300

    alterações

--- src/pages/Hall/index.js
+++ src/pages/Hall/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import './Hall.css';
+import React,{useState, useEffect} from 'react';
+import { useHistory } from 'react-router-dom'
 
 function Hall(){
+
+    const history = useHistory()
+
+    const routerBack = () => {
+        history.push('/hall')
+    }
+
+    const token  = localStorage.getItem("token");
+    
+    function cardapio (event) {
+        event.preventDefault();
+        event.preventDefault();
+
+                    fetch('https://lab-api-bq.herokuapp.com/products/', {
+                        method: 'GET',
+                        headers: {
+                            'accept': 'application/json',
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Authorization':`${token}`
+                        },
+
+                    })
+                        .then((response) => response.json()).then((json) => {
+                             console.log(json);
+                            if (json.id !== null) {
+                                routerBack();
+                            }
+                            
+                            
+                        })
+
+    }
+    
     return(
-        <h1>Página salão</h1>
+        <div className="App">
+           <h1>Cardápios</h1> 
+        
+        <button className="cardapioManha" onClick={cardapio}>Café da manhã</button>
+        <button className="cardapioTarde" onClick={cardapio}>Almoço / Jantar</button>
+        
+        
+        </div>    
     );
 };
 
