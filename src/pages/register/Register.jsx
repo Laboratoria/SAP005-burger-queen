2e4275e974118ae40de77c5f37e64d1eec558f33
Priscila <priscilassilveira97@gmail.com>
2021-02-16 17:19:24 -0300

    produtos da api

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Title, Form, Template, Page, Input,  Button, Images, BurgerImage} from '../../stylesForm';
