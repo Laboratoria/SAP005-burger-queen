fd833f45f8ac11a649f74e88a5fdf33236689938
carinarocha <71727396+carinarocha@users.noreply.github.com>
2021-02-16 21:37:44 -0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,23 +4,25 @@ import { Link } from 'react-router-dom';
 
 const STYLES = ['button-unClicked', 'button-Clicked'];
 
-const SIZES = ['button-medium', 'btn-large'];
+// futuro refeitório wanna be btn
+//const SIZES = ['button-medium', 'button-large'];
 
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize
+  //buttonSize
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)? buttonStyle: STYLES[0];
 
-  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+  //const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+  //${checkButtonSize}
 
   return (
     <Link to='/signup' className='button-devices'>
       <button
-        className={`button ${checkButtonStyle} ${checkButtonSize}`}
+        className={`button ${checkButtonStyle} `}
         onClick={onClick}
         type={type}
       >
