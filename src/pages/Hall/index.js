67618dba84a1486c15b00583353985b9043e4d33
Juliana <juliana.souzas@yahoo.com.br>
2021-02-18 14:20:35 -0300

    adicionado filtro para separar cardapio

--- src/pages/Hall/index.js
+++ src/pages/Hall/index.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
+import './Hall.css';
 
 
 
@@ -9,6 +10,7 @@ function Hall(){
         history.push('/')
     }
 
+    const [cafe, setCafe] = useState('');
     const [menu, setMenu] = useState('');
     const token  = localStorage.getItem("token");
 
@@ -22,7 +24,11 @@ function Hall(){
             },
         })
             .then((response) => response.json()).then((json) => {
-                setMenu(json)
+                const breakfast = json.filter(item => item.type === 'breakfast')
+                const allDay = json.filter(item => item.type === 'all-day')
+                setCafe(breakfast)
+                setMenu(allDay)
+                
                 console.log(json);
             })
     }, []);
@@ -31,6 +37,8 @@ function Hall(){
     return(
         <div className="App">
              <button className="btnExit" onClick={ routerBack}>Sair</button>
+
+             <div className="container-allDay">
           {
               menu && menu.map((item) => (
                   <div className="containerMenu">
@@ -40,6 +48,20 @@ function Hall(){
                   
               ))
           }
+          </div>
+
+          <div className="container-cafe">
+          {
+              cafe && cafe.map((item) => (
+                  <div className="containerMenu">
+                  <h1 className="divName">{item.name}</h1>,
+                  <h1 className="divPrice">R${item.price},00</h1>
+                 </div>
+                  
+              ))
+          }
+          </div>
+          
           
         </div>    
     );
