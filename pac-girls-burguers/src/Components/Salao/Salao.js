44e5d53430ef59680d6b6fe7f7cc367e86c18a67
Kaueny Alves <kaueny@MacBook-Pro-de-Kaueny.local>
2021-02-19 12:04:09 -0300

    styles: inicio do estilo da tela do salão

--- pac-girls-burguers/src/Components/Salao/Salao.js
+++ pac-girls-burguers/src/Components/Salao/Salao.js
@@ -4,11 +4,8 @@ import CardItem from "./CardItem";
 import styled from "styled-components";
 
 const Container = styled.div`
-
-
-`
-;
-
+  display: flex;
+`;
 const Comanda = styled.div`
   display: flex;
   flex-direction: column;
@@ -22,65 +19,70 @@ const Comanda = styled.div`
   height: 500px;
 `;
 
-
 const Salao = () => {
+  const [token, setToken] = useState();
 
-const [token,setToken] = useState() 
+  console.log(token);
 
-console.log(token)
+  useEffect(() => {
+    setToken(localStorage.getItem("token"));
+  }, []);
 
-  async function orders (token)  {
-    try { 
-      const body = {
-        "client": "carina",
-        "table": "05",
-        "products": [
-          {
-            "id": 29,
-            "qtd": 1
-          }
-        ]
-      }
-      const response = await fetch(
-        "https://lab-api-bq.herokuapp.com/orders" ,
-        {
-          method: "POST",
-          headers: {'accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-         Authorization: token
-          },
-           body
-          },
-      );
+  async function orders(token) {
+    try {
+      const response = await fetch("https://lab-api-bq.herokuapp.com/orders", {
+        method: "POST",
+        headers: {
+          Authorization: token,
+        },
+        body: {
+          client: "carina",
+          table: "05",
+          products: [
+            {
+              id: 29,
+              qtd: 1,
+            },
+          ],
+        },
+      });
       const json = await response.json();
-    
+
       console.log(json);
     } catch (error) {
       console.log(error);
     }
   }
-   useEffect(() => {
-    setToken(localStorage.getItem("token"))
-   }, [])
 
   return (
     <>
-      <CardItem />
       <Container>
-        <Comanda>
-          <h2>Comanda</h2>
-          <label>Cliente:</label>
-          <input placeholder="Nome do cliente" />
-          <br />
-          <label>Mesa:</label>
-          <input placeholder="Mesa" />
-          <br />
-          <p>pedidos...</p>
-          <li>ola</li>
-          <p>valor total: R$....</p>
-          <button onClick={()=>{orders(token)}}>enviar pedido</button>
-          <button>editar pedido</button>
-        </Comanda>
+        <div>
+          {" "}
+          <CardItem />
+        </div>
+        <div>
+          <Comanda>
+            <h2>Comanda</h2>
+            <label>Cliente:</label>
+            <input placeholder="Nome do cliente" />
+            <br />
+            <label>Mesa:</label>
+            <input placeholder="Mesa" />
+            <br />
+            <p>pedidos...</p>
+            <li>ola</li>
+            <p>valor total: R$....</p>
+            <button
+              onClick={() => {
+                orders(token);
+              }}
+            >
+              enviar pedido
+            </button>
+            <button>editar pedido</button>
+          </Comanda>
+        </div>
       </Container>
     </>
   );
