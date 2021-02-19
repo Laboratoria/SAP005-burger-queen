2e6ce46dfa7f80a9f14bdc2fa4df0d31dd38103a
RoSapia <RoSapia>
2021-02-19 18:55:38 -0300

    pegando token do usuario atual

--- src/pages/Lounge/CreateOrder.js
+++ src/pages/Lounge/CreateOrder.js
@@ -6,6 +6,7 @@ import './Lounge.css'
 export const CreateOrder = () => {
   const [client, setClient] = useState('');
   const [table, setTable] = useState('');
+  let token = localStorage.getItem('token')
   const handleClient = (event) => {
     setClient(event.target.value);
   }
@@ -23,7 +24,7 @@ export const CreateOrder = () => {
   }
 
   var myHeaders = new Headers();
-  myHeaders.append("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJvYmVydGFAYm5tLmNvbSIsImlkIjo2MDgsImlhdCI6MTYxMzY1OTQ4MSwiZXhwIjoxNjQ1MjE3MDgxfQ.4IK5_6Iz6q4cTqrKhU1q4_gXvefTAh75U_3aH8mxDZc");
+  myHeaders.append("Authorization", token);
   myHeaders.append("Content-Type", "application/json");
 
   //var raw = "";
@@ -43,7 +44,7 @@ export const CreateOrder = () => {
   const handleSendKitchen = () => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkthcmluYUBibm0uY29tIiwiaWQiOjMzNCwiaWF0IjoxNjEzMzQ3OTcwLCJleHAiOjE2NDQ5MDU1NzB9.a74Ol9lGMuWmk8zg6qMwh5nWn0cFwjnzBW52dc0V0xY");
+    myHeaders.append("Authorization", token);
 
     var raw = JSON.stringify({ "client": client, "table": table, "products": [{ "id": 29, "qtd": 1 }, { "id": 30, "qtd": 1 }, { "id": 40, "qtd": 1 }] });
 
@@ -71,18 +72,18 @@ export const CreateOrder = () => {
       <h1>Criar Pedido</h1>
       <div className="row">
         <div className="col">
-        <input type="text" class="form-control" placeholder="Cliente" aria-label="Cliente" onChange={handleClient}
+        <input type="text" className="form-control" placeholder="Cliente" aria-label="Cliente" onChange={handleClient}
             value={client}
             required/>
         </div>
         <div className="col">
-        <input type="text" class="form-control" placeholder="Mesa" aria-label="Mesa" onChange={handleTable}
+        <input type="text" className="form-control" placeholder="Mesa" aria-label="Mesa" onChange={handleTable}
             value={table}
             required/>
         </div>
       </div>
       <button id="back-btn" onClick={BackBtn}>Voltar</ button>
-      <button className="btn" onClick={handleSendKitchen}>Enviar para cozinha</ button>
+      <button className="btn" onClick={handleSendKitchen}>Enviar para cozinha</button>
     </div>
 
   )
