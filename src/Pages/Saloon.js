43204f682dbf2637d8470364effe6a972c566cd8
letbrasil <le.brasileiro@gmail.com>
2021-02-20 00:55:05 -0300

    ajusta indentação

--- src/Pages/Saloon.js
+++ src/Pages/Saloon.js
@@ -10,16 +10,16 @@ function Saloon() {
   const token = localStorage.getItem("token");
 
   useEffect(() => { 
-	  const myHeaders = new Headers();
-	  myHeaders.append("Authorization", `${token}`);
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `${token}`);
   
-	 	const requestOptions = {
-			method: "GET",
-			headers: myHeaders,
-			redirect: "follow"
-		};
+     const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    };
 
-		fetch("https://lab-api-bq.herokuapp.com/products", requestOptions) 
+    fetch("https://lab-api-bq.herokuapp.com/products", requestOptions) 
       .then(response => response.json())
       .then(result => {
         console.log(result);
@@ -28,12 +28,12 @@ function Saloon() {
       .catch(error => console.log("error", error));
   }, [])
 
-	function clientOrder(clientBox, table, products) {
-		const myHeaders = new Headers();
-		myHeaders.append("Authorization", `${token}`);
-		myHeaders.append("Content-Type", "application/json");
+  function clientOrder(clientBox, table, products) {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `${token}`);
+    myHeaders.append("Content-Type", "application/json");
 
-		const raw = JSON.stringify({"client":{clientBox},"table":{table},"products":[{products}]});
+    const raw = JSON.stringify({"client":{clientBox},"table":{table},"products":[{products}]});
 
     const requestOptions = {
       method: "POST",
@@ -42,18 +42,18 @@ function Saloon() {
       redirect: "follow"
     };
 
-		fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
+    fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
       .then(response => response.json())
       .then(result => console.log(result))
       .catch(error => console.log("error", error));
-	}
-		
-	/*function handleSubmit (event) {
-		event.preventDefault();
-		clientOrder (clientBox, table, products)
-	}*/
-		
-	return (
+  }
+    
+  /*function handleSubmit (event) {
+    event.preventDefault();
+    clientOrder (clientBox, table, products)
+  }*/
+    
+  return (
     <div className="saloon-page">
       <h1>Salão</h1>
         <div className="anote-pedido">
@@ -89,7 +89,7 @@ function Saloon() {
           })
         }</div>
     </div>
-	)
+  )
 }
 
 export default Saloon;
