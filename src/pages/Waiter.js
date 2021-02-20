168ef7e99868b0da78e71c8f2d66cc27ea88458b
Tânagra Andria <design.andria@gmail.com>
2021-02-20 13:38:58 -0300

    CODE: botões do menu com imagem, div do pedido

--- src/pages/Waiter.js
+++ src/pages/Waiter.js
@@ -2,12 +2,16 @@ import '../style/Waiter.css';
 import { useEffect, useState } from 'react';
 import {useHistory} from 'react-router-dom'
 import logo from '../images/logo.png';
+import prato from '../images/prato.png';
+import xicara from '../images/xicara.png';
 import exit from '../images/exit.jpeg';
 
 const Waiter = () => {
 
   const [menu, setMenu] = useState([]);
   const [allDay, setDay] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
   const token = localStorage.getItem("token");
 
   console.log(allDay)
@@ -46,53 +50,70 @@ const Waiter = () => {
         <button className="exit"   onClick={rLogin}>
       <img src= {exit} alt="" className="exit"/></button>
       </nav>
-      <header className="AppWaiter">
-        <div>
-        <img src={logo} alt="" className="logoWaiter" />
+
+      <header className="App-waiter">
+       <div id="logoWaiter"> 
+        <img src={logo} alt="" className="logoWaiter" /> 
         </div>
-        <button className='breakfast'onClick={( (e) => {
+      <ol>
+        <section className='Menu'>
+            <button className="btnMenu"   onClick={((e)=>{
+              e.preventDefault();
+              console.log(menu)
+            })}><img src= {xicara} alt="" className='imgMenu' /></button>
+
+            <button className="btnMenu"   onClick={((e)=>{
+              e.preventDefault();
+              console.log(menu)
+            })}><img src= {prato} alt="" className='imgMenu' /></button>
+
+        
+              <div className='menuItens'> {
+                menu.map((menuItems) => {
+
+                  return (
+                    <div key={menuItems.id}>
+                      <p>{menuItems.name}</p>
+                      <p>{menuItems.flavor}</p>
+                      <p>R$:{menuItems.price},00</p>
+                    </div>
+                  )
+                })
+              } </div>
+
+            <button className='allDay'onClick={( (e) => { 
               e.preventDefault();
               console.log('clicou');
-              setMenu([{id:'fruta', name:'banana', price:'5,00', flavor:'vegetariano'}])
-        })}
-        >breakfast</button>
+              <div className='menuItens'> {
+                allDay.map ((menuItems)=>{
+                  return (
+                    <div key={menuItems.id}>
+                      <p>{menuItems.name}</p>
+                      <p>{menuItems.flavor}</p>
+                      <p>R$:{menuItems.price},00</p>
+                    </div>
+                  )
+                })
+              }</div>
+            }
+          )}>Refeição</button>
+        </section>
 
-        <div className='menuItens'> {
-          menu.map((menuItems) => {
+        <form className='order'>
+          <h1>Pedido</h1>
+            <input type="text" id="client" placeholder="Digite o nome do cliente" value={name} onChange={(event) =>
+              setName(event.target.value)} />
+            <input type="number" id="number" min='0' max='20' value={number} onChange={(event) =>
+              setNumber(event.target.value)} />
+          </form>
 
-            return (
-              <div key={menuItems.id}>
-                <p>{menuItems.name}</p>
-                <p>{menuItems.flavor}</p>
-                <p>R$:{menuItems.price},00</p>
-              </div>
-            )
-          })
-        } </div>
-      {/* })} >CAFÉ DA MANHÃ</button> */}
-        
-        {/* <button className='allDay'onClick={( (e) => {
-              e.preventDefault();
-              console.log('clicou'); */}
-          {/* <div className='menuItens'> {
-          allDay.map((menuItems) => {
-
-            return (
-              <div key={menuItems.id}>
-                <p>{menuItems.name}</p>
-                <p>{menuItems.flavor}</p>
-                <p>R$:{menuItems.price},00</p>
-              </div>
-            )
-          })
-        } </div> */}
-      {/* })} >REFEIÇÃO</button> */}
-
-
-
+       </ol>
+       <button className='send'>ENVIAR</button>
       </header>
     </div>
   )
 }
+
+
 
 export default Waiter;
