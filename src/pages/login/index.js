7d2aff3659fa08082da619e4dc5afc7ea72f6189
edilene <edilenefern@gmail.com>
2021-02-22 17:11:51 -0300

    role

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -5,9 +5,18 @@ function Login() {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // const [role, setRole] = useState("");
+  // const role = setRole();
 
-  const routerLogin = () => {
-    history.push("/Hall");
+  // const routerLogin = () => {
+  //   history.push("/");
+  // }
+  const routerKitchen = () => {
+    history.push("/kitchen");
+  };
+
+  const routerHall = () => {
+    history.push("/hall");
   };
 
   const handleClick = (event) => {
@@ -16,51 +25,52 @@ function Login() {
     fetch("https://lab-api-bq.herokuapp.com/auth", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
-        
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({email,password})
+      body: JSON.stringify({ email, password }),
     })
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-        routerLogin();
+        if (json.role === "hall") {
+          routerHall();
+        }
+        if (json.role === "kitchen") {
+          routerKitchen();
+        }
       });
-  
   };
-
 
   return (
     <div>
       <h1>Login</h1>
       <form className="login" onSubmit={handleClick}>
-
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email">Email:</label>
         <input
           className="login"
-          type="email"
+          type="Email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
+          required
         />
-
-        <label htmlFor="passowrd">Senha </label>
+        <p />
+        <label htmlFor="passowrd">Senha: </label>
         <input
           className="login"
           type="password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
+          required
         />
-          
+        <p />
 
-
-        <label htmlFor="login">Login</label>
+        <label htmlFor="login">Login:</label>
         <button type="submit" id="button" onClick={handleClick}>
           Entrar
-         </button>
+        </button>
 
         <p>Não tem uma conta?</p>
         <Link to="/Register">Cadastrar</Link>
-       
       </form>
     </div>
   );
