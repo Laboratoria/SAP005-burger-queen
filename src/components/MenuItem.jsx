6983832ac23995d409f5cc61d1af6803a1898678
Nijoane <nicajoane@outlook.com>
2021-02-22 18:00:19 -0300

    atualizando MenuItem

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,6 +1,5 @@
 function MenuItem({name, flavor, complement, price, addToCart, sku}) {
     return(
-      
         <div class="menu-item">
             <span className="menu-text">{`${name} ${flavor} ${complement} - R$ ${price},00`}</span>
             <span className="menu-action">
