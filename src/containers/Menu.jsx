81a7e59d462dc5fa838db6e63138434552d1bb01
Nijoane <nicajoane@outlook.com>
2021-02-22 19:35:16 -0300

    [fixed] key error

--- src/containers/Menu.jsx
+++ src/containers/Menu.jsx
@@ -7,9 +7,7 @@ function Menu({ menu, addToCart }) {
 
     return (
         <div>
-
             <div id="menu-area">
-              
                     <DivButton>
                         <ButtonMenu onClick={() => setShow(true)}>Café da Manhã</ButtonMenu>
                         <ButtonMenu onClick={() => setShow(false)}>Hamburgueria</ButtonMenu>
@@ -20,13 +18,14 @@ function Menu({ menu, addToCart }) {
                     {
                         show ?
                             <div>
-                                {Object.keys(menu).slice(0, 4).map((key) => (
+                                {Object.keys(menu).slice(0, 4).map((key, index) => (
                                     <MenuItem
                                         name={menu[key].name}
                                         flavor={[]}
                                         complement={[]}
                                         price={menu[key].price}
                                         addToCart={addToCart}
+                                        key={index}
                                         sku={key}
                                     />
                                 ))
@@ -34,13 +33,14 @@ function Menu({ menu, addToCart }) {
                             </div>
                             :
                             <div>
-                                {Object.keys(menu).slice(4, 28).map((key) => (
+                                {Object.keys(menu).slice(4, 28).map((key, index) => (
                                     <MenuItem
                                         name={menu[key].name}
                                         flavor={menu[key].flavor}
                                         complement={menu[key].complement}
                                         price={menu[key].price}
                                         addToCart={addToCart}
+                                        key={index}
                                         sku={key}
                                     />
                                 ))
