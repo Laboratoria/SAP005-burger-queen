11f815bf03243e502a41aec569a9a11473298a90
Carol Shimada <caroline_shimada@hotmail.com>
2021-02-22 21:30:44 -0300

    corrigindo a funcao menu

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,112 +1,91 @@
-import React from 'react';
-import {NavBar} from '../../components.js';
-import {Grid} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input'; 
+import {Copyright, Logo, useStyles, NavBar} from '../../components.js';
 import { IconButton } from '@material-ui/core';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import Input from '@material-ui/core/Input';
+import { Link } from 'react-router-dom';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import { Link} from 'react-router-dom';
 
 
-class List extends React.Component {
+const Menu = () => {
+  const classes = useStyles();
+  const tokenLocal = localStorage.getItem('token');
 
-  state = {
-    ArrayBreakfast: [],//arrayProducts
-    ArrayAllDay: []
-  };
-  
-  componentDidMount() {
-    
-    const tokenLocal = localStorage.getItem('token');
-      fetch('https://lab-api-bq.herokuapp.com/products'
-      , {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': `${tokenLocal}` ,
-        },
+
+  const [breakefast, setBreakfast] = useState([]);
+  const [hamburguers, setHamburguers] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    fetch('https://lab-api-bq.herokuapp.com/products', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `${tokenLocal}`
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        const products = data;
+        console.log(data);
+
+        const productsBreakfast = products.filter(itens => itens.type===('breakfast'));
+        setBreakfast(productsBreakfast);
+
+        const productsHamburguers = products.filter(itens => itens.sub_type===('hamburguer'));
+        setHamburguers(productsHamburguers);
+        
+        const productsDrinks = products.filter(itens => itens.sub_type===('drinks'));
+        setDrinks(productsDrinks);
       })
-          .then((res) => res.json())
-          .then((json) => {
-            const breakfast = json.filter(item => item.type==='breakfast')
-            const allDay = json.filter(item => item.type==='all-day')
-              this.setState({
-                  ArrayBreakfast: breakfast,
-                  ArrayAllDay: allDay
-              });
-          });
-          
-  } 
-  
-  render() {
-      return (
-        <React.Fragment>
-          <NavBar/>
-          <Grid 
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="flex-start"
-          color='white'
-          >
-            <Grid  id='menu'>
-                {/* <Button onClick={
-                  <div> */}
-                <h1>Café da manhã</h1>
-                <ul> 
-                {this.state.ArrayBreakfast.map(item => (                    
-                    <li key={item.id}>
-                        <p>{item.name} {item.flavor} {item.complement} R${item.price},00<IconButton color="primary" aria-label="add to shopping cart"><AddShoppingCartIcon/></IconButton></p>                                                         
-                    </li>
-                ))}
-                </ul>
-                  <ul><h1>Resto do dia</h1>
-                    {this.state.ArrayAllDay.map(item => (
-                        <li key={item.id}>
-                            <p> {item.name} {item.flavor} {item.complement} R${item.price},00<IconButton color="primary" aria-label="add to shopping cart"><AddShoppingCartIcon/></IconButton></p>                                                       
-                        </li>
-                    ))}
-                </ul>
-            </Grid>
-            <Grid>
-              <h1>Pedidos</h1>
-              <form noValidate autoComplete="off"justify="flex-start">
-                <Input placeholder="Nome"inputProps={{ 'aria-label': 'description' }} />
-                <Input placeholder="Mesa" inputProps={{ 'aria-label': 'description' }} />
-              </form  >
-            </Grid>
-          </Grid>
-          <Link to="/Hall"><ArrowBackIosIcon color="disabled" fontSize="large"/> </Link>
-        </React.Fragment>
-      );
-  }
-}
+  }, [])
 
-export default List;
+  return (
+    <div>
+      <Grid
+       className='container'>
+        <NavBar />
+        <div className={classes.paper} >
+        <form className={classes.paperTable}  noValidate autoComplete="off" >
+              <Input placeholder="Nome"fullWidth inputProps={{ 'aria-label': 'description' }} />
+              <Input placeholder="Mesa" inputProps={{ 'aria-label': 'description' }} />
+            </form>
+        </div>
 
+        <div>
+          <div>
+            <h1>Café</h1>
+            {breakefast.map((produto) => (
+              <div key={produto.id}>
+                <p>{produto.name} R$ {produto.price},00 {produto.complement} <IconButton color="primary" aria-label="add to shopping cart"><AddShoppingCartIcon/></IconButton></p>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h1>Resto do dia</h1>
+            {hamburguers.map((produto) => (
+              <p key={produto.id}>
+                <p>{produto.name} {produto.flavor} {produto.complement} R$ {produto.price},00<IconButton color="primary" aria-label="add to shopping cart"><AddShoppingCartIcon/></IconButton></p>
+              </p>
+            ))}
+          </div>
+          <div>
+            <h1>Bebidas</h1>
+            {drinks.map((produto) => (
+              <p key={produto.id}>
+                <p>{produto.name} R$ {produto.price},00<IconButton color="primary" aria-label="add to shopping cart"><AddShoppingCartIcon/></IconButton></p>
+              <p>
+                </p>
+              </p>
+            ))}
+          </div>
+        </div>
+      </Grid>
+      <Link to="/Hall"><ArrowBackIosIcon color="disabled" fontSize="large"/> </Link>
+      <Copyright />
+    </div>
+  )
+};
 
-
-
-
-
-
-//import ReactDOM from 'react-dom';
-//import { Link } from 'react-router-dom';
-//import {useStyles, Copyright, NavBar} from '../../components.js';
-//import Button from '@material-ui/core/Button';
-//import Input from '@material-ui/core/Input';
-//import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-//import Container from '@material-ui/core/Container';
-//import { SettingsRemoteRounded } from '@material-ui/icons';
-//import MyComponent from '../../configApi/Api'
-//import { RecentActorsOutlined } from '@material-ui/icons';
-//import { useHistory, useParams } from "react-router-dom";
-
-
-
-
-
-
-
-
-
+export default Menu;
