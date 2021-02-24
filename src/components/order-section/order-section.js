6f1114a8be2177e88eb726bcb5fbd892e90a1ec9
caroline costa <carolmarcionilia@gmail.com>
2021-02-23 21:33:50 -0300

    add folder and file

---
+++ src/components/order-section/order-section.js
@@ -0,0 +1,12 @@
+import React, { Fragmente } from 'react'
+import OrderItens from '../order-itens/order-itens'
+import './order-section.css'
+
+export default function OrderSection({
+
+}) {
+  return (
+    <Fragment>
+    </Fragment>
+  )
+}
