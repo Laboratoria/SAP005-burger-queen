570f503b52370880e7362325124bde1b537adc86
Priscila <priscilassilveira97@gmail.com>
2021-02-24 11:46:19 -0300

    icone de excluir produtos

--- src/containers/Cart.jsx
+++ src/containers/Cart.jsx
@@ -1,3 +1,4 @@
+import { FaTrashAlt } from 'react-icons/fa';
 import { Textarea, SpamQtd, ProductsOrders, SpanNameOrders, SpanFlavor, DivTotal, Complement, Soma, Total, Itens } from '../components/stylesMenu';
 function Cart({ total, cart, menu, reduceCart }) {
     return (
@@ -17,9 +18,17 @@ function Cart({ total, cart, menu, reduceCart }) {
                             <SpanNameOrders className="item-text">
                                 <Itens>{menu[sku].name}</Itens>
                                 <Itens>{cart[sku]}</Itens>
-                                <span className="item-total"> 
-                                R$ {menu[sku].price},00 
-                                <button className="remove-button" onClick={() => reduceCart(sku)}>-</button>
+                                <span className="item-total">
+                                    R$ {menu[sku].price},00
+                                <button className="remove-button" onClick={() => reduceCart(sku)}
+                                        style={{
+                                            'border': 'none',
+                                            'outline': 'none',
+                                            'backgroundColor': 'transparent',
+                                            'padding': '5px',
+                                            'color': '#E65100',
+                                            'cursor': 'pointer'
+                                        }}><FaTrashAlt /></button>
                                 </span>
                             </SpanNameOrders>
                             <SpanFlavor>
--- src/containers/Menu.jsx
+++ src/containers/Menu.jsx
@@ -7,9 +7,7 @@ function Menu({ menu, addToCart }) {
 
     return (
         <div>
-
             <div id="menu-area">
-              
                     <DivButton>
                         <ButtonMenu onClick={() => setShow(true)}>Café da Manhã</ButtonMenu>
                         <ButtonMenu onClick={() => setShow(false)}>Hamburgueria</ButtonMenu>
@@ -27,8 +25,8 @@ function Menu({ menu, addToCart }) {
                                         complement={[]}
                                         price={menu[key].price}
                                         addToCart={addToCart}
-                                        sku={key}
                                         key={index}
+                                        sku={key}
                                     />
                                 ))
                                 }
@@ -36,15 +34,15 @@ function Menu({ menu, addToCart }) {
                             :
                             <div>
                                 {Object.keys(menu).slice(4, 28).map((key, index) => (
-                                    
+
                                     <MenuItem
                                         name={menu[key].name}
                                         flavor={menu[key].flavor}
                                         complement={menu[key].complement}
                                         price={menu[key].price}
                                         addToCart={addToCart}
-                                        sku={key}
                                         key={index}
+                                        sku={key}
                                     />
                                 ))
                                 }
