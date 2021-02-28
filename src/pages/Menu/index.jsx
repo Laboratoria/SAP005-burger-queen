2e4269beaa84d2f0c219042482a17fc159b56326
Carol Shimada <caroline_shimada@hotmail.com>
2021-02-28 17:10:01 -0300

    primeira tentativa de pedidos

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,84 +1,224 @@
 import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Input from '@material-ui/core/Input'; 
-import {Copyright, Logo, useStyles, NavBar} from '../../components.js';
-import { IconButton } from '@material-ui/core';
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import {Copyright, useStyles, NavBar} from '../../components.js';
 import { Link } from 'react-router-dom';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 
+
 const Menu = () => {
-  const classes = useStyles();
-  const tokenLocal = localStorage.getItem('token');
-  
-  const [list, setList] = useState([]);
-  const [listMap, setListMap] = useState([]);
+const classes = useStyles();
+const tokenLocal = localStorage.getItem('token');
+console.log(tokenLocal)
+const [list, setList] = useState([]);
+const [listMap, setListMap] = useState([]); 
+const [totalOrder, setTotalOrder] = useState([]);
+const [order, setOrder] = useState({});
+const [deletedProduct, setDeletedProduct] = useState([]);
+const [totalPrice, setTotalPrices] = useState([]);
+const [productsPrices, setProductsPrices] = useState([]);
 
-  useEffect(() => {
-    fetch('https://lab-api-bq.herokuapp.com/products', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `${tokenLocal}`
-      },
+useEffect(() => {
+  fetch('https://lab-api-bq.herokuapp.com/products', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${tokenLocal}`
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setList(data) 
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setList(data) 
-      })
-  }, [])
+}, [tokenLocal])
 
-  const listBreakFast = () => {
-    console.log(list)
+//inserir order
+const postOrder = () => {
+  fetch('https://lab-api-bq.herokuapp.com/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${tokenLocal}`,
+    },
+    body: JSON.stringify(order),
+  })
+    .then((response) => {
+      response.json()
+        .then(() => {
+          //console.log(data);
+          setOrder({});
+          setTotalOrder([]);
+          setTotalPrices([]);
+          setProductsPrices([]);
+          setDeletedProduct([]);
+          alert('deu certo');
+        });
+    })
+    .catch(() => {
+      alert("deu ruim");
+    });
+};
 
-    const productsBreakfast = list.filter(itens => itens.type===('breakfast'));
-      setListMap(productsBreakfast); 
+const listBreakFast = () => {
+  console.log(list)
+
+  const productsBreakfast = list.filter(itens => itens.type===('breakfast'));
+    setListMap(productsBreakfast); 
+}
+
+const listHamburguer = () => {
+  console.log(list)
+  const productsHamburguers = list.filter(itens => itens.sub_type===('hamburguer'));
+    setListMap(productsHamburguers);
+    console.log(productsHamburguers)
+}
+
+const listDrinks = () => {
+  console.log(list)
+  const productsDrinks = list.filter(itens => itens.sub_type===('drinks'));
+    setListMap(productsDrinks);
+}
+const addItems = (product) => {
+  setTotalOrder([...totalOrder, product]);
+  setProductsPrices([...productsPrices, product.price]);
+  const quantityOrder = totalOrder.map((product) => {
+    return {
+      id: product.id,
+      qtd: 1,
+    };
+  });
+  // {
+  //   "client": "string",
+  //   "table": "string",
+  //   "products": [
+  //     {
+  //       "id": 0, id do produto
+  //       "qtd": 0 quantidade para add
+  //     }
+  //   ]
+  // }
+
+  const qtd = quantityOrder.reduce(function (array, productItem) {
+    array[productItem.id] = array[productItem.id] || [];
+    array[productItem.id].push(productItem);
+    return array;
+  }, Object.create(null));//criar um novo objeto
+
+  /*Uso de reduce
+  ex:
+  cont rockets = [
+  {country: "Russia", Lauches: 32"},
+  {country: "US", Lauches: 23"},
+  {country: "Chiva!, Lauches: 16"},
+  {country: "Europe", Lauches: 7"},
+  {country: "india!, Lauches: 4"},
+  {country: "Japan", Lauches: 3"}
+  ]
+  const totalLauches = rockets.reduce((valoranterior, numerolanatual) => valoranterior + numeroatual.lauches, 0(numero inicial para contagem))
+  vai dar a soma dos lauches
+  */
+  const arrayProducts = [];
+  for (const [key, value] of Object.entries(qtd)) {
+    arrayProducts.push({
+      id: key,
+      qtd: value.length,
+    });
   }
 
-  const listHamburguer = () => {
-    console.log(list)
-    const productsHamburguers = list.filter(itens => itens.sub_type===('hamburguer'));
-      setListMap(productsHamburguers);
-  }
+  setOrder({ ...order, products: arrayProducts });
+};
+//splice insere ao array
+const deletingProducts = (product) => {
+  setTotalPrices(productsPrices.splice(totalOrder.indexOf(product), 1));
+  setDeletedProduct(totalOrder.splice(totalOrder.indexOf(product), 1));
+  sumFunction();
+};
 
-  const listDrinks = () => {
-    console.log(list)
-    const productsDrinks = list.filter(itens => itens.sub_type===('drinks'));
-      setListMap(productsDrinks);
-    
-  }
+const sumFunction = () => {
+  setTotalPrices(productsPrices.reduce((resume, num) => resume + num));
+};
 
-  return (
-    <div>
-      <Grid
-       className='container'>
+return (
+  <div>
+    <Grid id='menuList'className='container'   
+    container
+    direction="row"
+    justify="flex-start"
+    alignItems="flex-start">
+      <Grid>
         <NavBar />
-        <div className={classes.paper} >
-        <form className={classes.paperTable}  noValidate autoComplete="off" >
-              <Input placeholder="Nome"fullWidth inputProps={{ 'aria-label': 'description' }} />
-              <Input placeholder="Mesa" inputProps={{ 'aria-label': 'description' }} />
-            </form>
-        </div>
-
         <div>
           <div>
-            <button onClick={listBreakFast}>Café da manhã</button>
-            <button onClick={listHamburguer}>Hamburguers</button>
-            <button onClick={listDrinks}>Bebidas</button>
-            {listMap.map((produto) => (
-              <div key={produto.id}>
-                <p>{produto.name} R$ {produto.price},00 {produto.complement} <IconButton color="primary" aria-label="add to shopping cart"><AddShoppingCartIcon/></IconButton></p>
+            <button onClick={listBreakFast} className={classes.submitMenuType}>Café da manhã</button>
+            <button 
+            onClick={listHamburguer} 
+            className={classes.submitMenuType}>Hamburguers</button>
+            <button 
+            onClick={listDrinks} 
+            className={classes.submitMenuType} >Bebidas</button>
+            {listMap.map((product) => (
+              <div key={product.id}>
+                <button className={classes.submitMenuItems}  
+                onClick={() => addItems(product)} >{product.name} {product.flavor} {product.complement}<br></br>R$ {product.price},00  </button>
               </div>
             ))}
           </div>
         </div>
+      </Grid >  
+      <Grid id='orderResume'className='container'name={tokenLocal.name}>
+        Pedido
+        <section className={classes.paper} >
+          <form className={classes.paperTable}  noValidate autoComplete="off" >
+            <Input placeholder="Nome"fullWidth inputProps={{ 'aria-label': 'description' }} type='text'
+            name='nome'
+            className='input'
+            required
+            onChange={(event) =>
+              setOrder({ ...order, client: event.target.value })
+            }/>
+            <Input placeholder="Mesa" inputProps={{ 'aria-label': 'description' }} type='text'
+            name='mesa'
+            className='input'
+            required
+            onChange={(event) =>
+              setOrder({ ...order, table: event.target.value })
+            } />
+          </form>
+        </section>
+            {totalOrder.map((product, index) => (
+            <div key={index}>
+              
+              <span>1 {product.name} </span>
+              <span>{product.flavor === 'null' ? '' : product.flavor} </span>
+              <span>{product.complement === 'null' ? '' : product.complement }</span>
+              <span> R$ {product.price},00  </span>
+              <span>
+                <span style={{ cursor: 'pointer' }} type='button'  onClick={() => deletingProducts(product)}>🗑️</span>
+              </span>
+            </div>
+          ))} 
+        <div >
+          <span >
+            <span>Total:</span>
+          </span>
+          <span>
+            <span>R$ {totalPrice},00</span>
+          </span>
+          <span>
+            <br></br><button onClick={() => sumFunction()}>Atualizar Total</button>
+          <br></br></span>
+          <span>
+            <button onClick={() => postOrder()}>Enviar para cozinha</button>
+          </span>
+        </div><br></br>
       </Grid>
-      <Link to="/Hall"><ArrowBackIosIcon color="disabled" fontSize="large"/> </Link>
-      <Copyright />
-    </div>
-  )
+    </Grid>
+    <Link to="/Hall"><ArrowBackIosIcon color="disabled" fontSize="large"/> </Link>
+    <Copyright />
+  </div>
+)
 };
 
 export default Menu;
