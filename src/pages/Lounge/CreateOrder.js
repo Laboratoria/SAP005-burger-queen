e920bff6717aae5d06db3e65b66b63b6b1428229
RoSapia <RoSapia>
2021-02-28 11:51:04 -0300

    estilizando comanda

--- src/pages/Lounge/CreateOrder.js
+++ src/pages/Lounge/CreateOrder.js
@@ -176,115 +176,133 @@ export const CreateOrder = () => {
             required />
         </div>
       </div>
-        {loading ?
-          (
-            <p>Carregando</p>
-          ) : (
-            <>
+      {loading ?
+        (
+          <p>Carregando</p>
+        ) : (
+          <>
 
-              <button onClick={() => setMenus(true)}>Café da Manhã</button>
+            <button onClick={() => setMenus(true)}>Café da Manhã</button>
 
-              <button onClick={() => setMenus(false)}>Comum</button>
+            <button onClick={() => setMenus(false)}>Comum</button>
 
-              {menus ? (
+            {menus ? (
+              <table className="menuList">
+                <tbody>
+                  <tr>
+                    <th>Produto</th>
+                    <th>Preço</th>
+                  </tr>
+                  {breakfastMenu.map((products) => (
+                    <tr key={products.id}>
+                      <td>{products.name}</td>
+                      <td> R${products.price}</td>
+                      <td>
+                        <button
+                          onClick={() => {
+                            handleAdd(products)
+                          }}>+</button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
                 <table className="menuList">
                   <tbody>
                     <tr>
                       <th>Produto</th>
+                      <th>...</th>
                       <th>Preço</th>
                     </tr>
-                    {breakfastMenu.map((products) => (
+                    {allDayMenu.map((products) => (
                       <tr key={products.id}>
-                        <td>{products.name}</td>
-                        <td> R${products.price}</td>
+                        <td>{products.name + ' ' + products.flavor}</td>
+                        <td>{products.complement === 'null' ? '' : products.complement}</td>
+                        <td>R${products.price}</td>
                         <td>
                           <button
-                            onClick={() => {
-                              handleAdd(products)
-                            }}>+</button>
+                            onClick={() =>
+                              handleAdd(products)}>+
+                          </button>
                         </td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
-              ) : (
-                  <table className="menuList">
-                    <tbody>
-                      <tr>
-                        <th>Produto</th>
-                        <th>...</th>
-                        <th>Preço</th>
-                      </tr>
-                      {allDayMenu.map((products) => (
-                        <tr key={products.id}>
-                          <td>{products.name + ' ' + products.flavor}</td>
-                          <td>{products.complement === 'null' ? '' : products.complement}</td>
-                          <td>R${products.price}</td>
-                          <td>
-                            <button
-                              onClick={() =>
-                                handleAdd(products)}>+
-                          </button>
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                )}
-               <h1>Comanda</h1> 
-              <table className="menuList">
-                <tbody>
-                  <tr>
-                    <th>Produto</th>
-                    <th>Complemento</th>
-                    <th></th>
-                    <th>Preço</th>
-                  </tr>
-                  {orderSummary.map((products, index) => (
-                    <tr key={index}>
-                      <td>{products.name}</td>
-                      <td>{products.flavor === 'null' ? '' : products.flavor}</td>
-                      <td>{products.complement === 'null' ? '' : products.complement}</td>
-                      <td>R${products.price}</td>
-                      <td>
-                        <button onClick={() => handleDeleteProducts(products)}>
+              )}
+            
+            <table className="order">
+              <tbody>
+                <thead>
+                <tr>
+                  <th colspan='4'><h1>Comanda</h1></th>
+                </tr>
+                </thead>
+                <tr>
+                  <th>Produto</th>
+                  <th>Complemento</th>
+                  <th></th>
+                  <th>Preço</th>
+                </tr>
+                
+                {orderSummary.map((products, index) => (
+                  <tr key={index}>
+                    <td>{products.name}</td>
+                    <td>{products.flavor === 'null' ? '' : products.flavor}</td>
+                    <td>{products.complement === 'null' ? '' : products.complement}</td>
+                    <td>R${products.price}</td>
+                    <td>
+                      <button onClick={() => handleDeleteProducts(products)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                         </svg>
-                        
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                  <th>TOTAL: R${totalPrice}</th>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th></th>
                   <th>
-                  <button className="btn" onClick={() => handleSum()}>SOMAR</button>
+                    <button className="btn" onClick={() => handleSum()}>SOMAR</button>
                   </th>
-                  
+                </tr>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th>TOTAL:</th>
+                  <th> R${totalPrice}</th>
+                </tr>
+                <tr>
+
+
                   <th>
-                   <button
-                    className="btn"
-                    type="button"
-                    value="Excluir pedido"
-                    onClick={() => {
-                      setTotalPrice([0]);
-                      setOrderSummary([]);
-                      //setOrderProducts([]);
-                    }}>
-                    excluir comanda</button>
+                    <button
+                      className="btn"
+                      type="button"
+                      value="Excluir pedido"
+                      onClick={() => {
+                        setTotalPrice([0]);
+                        setOrderSummary([]);
+                        //setOrderProducts([]);
+                      }}>
+                      excluir comanda</button>
                   </th>
-        <th>      
-        <button id="back-btn"  className="btn" onClick={BackBtn}>Voltar</ button>
-        </th>
-        <th>
-        <button className="btn" onClick={handleSendKitchen}>Enviar para cozinha</button>
-        </th>
-        </tbody>
-      </table> 
-    </>)}
+                  <th>
+                    <button id="back-btn" className="btn" onClick={BackBtn}>Voltar</ button>
+                  </th>
+                  <th>
+                    <button className="btn" onClick={handleSendKitchen}>Enviar para cozinha</button>
+                  </th>
+                </tr>
+              </tbody>
+            </table>
+          </>)}
     </div>
-  
+
 
   )
 };
