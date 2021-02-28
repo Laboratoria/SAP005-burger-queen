226d66d2736c2140bca42d8cf44f6341d408e2b4
RoSapia <RoSapia>
2021-02-27 23:39:46 -0300

    adicionando botão excluir, da erro no sum

--- src/pages/Lounge/CreateOrder.js
+++ src/pages/Lounge/CreateOrder.js
@@ -17,6 +17,7 @@ export const CreateOrder = () => {
   const [order, setOrder] = useState({});
   const [productsPrice, setProductsPrice] = useState([]);
   //const [placeOrder, setplaceOrder] = useState({ "table": table });
+  const [excludedProduct, setExcludedProduct] = useState([]);
 
   const [menus, setMenus] = useState(true);
 
@@ -107,6 +108,12 @@ export const CreateOrder = () => {
 
   };
 
+  const handleDeleteProducts = (products) => {
+    setTotalPrice(productsPrice.splice(orderSummary.indexOf(products), 1));
+    setExcludedProduct(orderSummary.splice(orderSummary.indexOf(products), 1));
+    handleSum();
+  };
+
   const handleSum = () => {
     setTotalPrice(productsPrice.reduce((total, num) => total + num));
     console.log('productsPrice', productsPrice)
@@ -137,6 +144,7 @@ export const CreateOrder = () => {
         setOrderSummary([]);
         setTotalPrice([0]);
         setProductsPrice([]);
+        setExcludedProduct([]);
         //setOrderProducts([]);
       }
       )
@@ -223,6 +231,7 @@ export const CreateOrder = () => {
                     </tbody>
                   </table>
                 )}
+               <h1>Comanda</h1> 
               <table className="menuList">
                 <tbody>
                   <tr>
@@ -231,13 +240,21 @@ export const CreateOrder = () => {
                     <th></th>
                     <th>Preço</th>
                   </tr>
-
                   {orderSummary.map((products, index) => (
                     <tr key={index}>
                       <td>{products.name}</td>
                       <td>{products.flavor === 'null' ? '' : products.flavor}</td>
                       <td>{products.complement === 'null' ? '' : products.complement}</td>
                       <td>R${products.price}</td>
+                      <td>
+                        <button onClick={() => handleDeleteProducts(products)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                        </svg>
+                        
+                        </button>
+                      </td>
                     </tr>
                   ))}
                   <th>TOTAL: R${totalPrice}</th>
