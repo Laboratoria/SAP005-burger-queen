ec0182ada5f04223618d95d99a8be8a2011f7851
elis-ctrl <elis_emb@hotmail.com>
2021-02-28 23:16:51 -0300

    settings

--- src/components/OrderInfo/index.js
+++ src/components/OrderInfo/index.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { FaUserAlt } from 'react-icons/fa'
-// import { getOrders } from '../../services/index'
+import { getOrders } from '../../services/index'
 import Navbar  from '../Navbar/Navbar.js'
 import Footer from '../Footer/Footer.js'
 // import Button from './Button/Button.js'
@@ -8,35 +8,35 @@ import './orderinfo.css'
 
 export const OrderInfo = () => {
   
-  return (
-    <Fragment>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <section className='card-order-info'>
+return (
+  <Fragment>
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <section className='card-order-info'>
 
-          {/* <p className='order-status'>{getOrders.status}</p>
+        <p className='order-status'>{getOrders.status}</p>
 
-          <div className='order-info'>
-          <p className='table'>Mesa:{getOrders.table}</p>
-          < FaUserAlt className='icon' /> <p className='client-name'>{getOrders.client_Name}</p>
-          <div className='order-products'>
-            <p className='products-name'>{getOrders.products.qtd}</p>
-            <p className='products-price'>{getOrders.products.name}</p>
-          </div> */}
-          {/* <Button>
-            name='delivery'
-            className='btn-order'
-            type='submit'
-            onClick={}
-          </Button> */}
-          </div>
+        <div className='order-info'>
+        <p className='table'>Mesa:{getOrders.table}</p>
+        < FaUserAlt className='icon' /> <p className='client-name'>{getOrders.client_Name}</p>
+        <div className='order-products'>
+          <p className='products-name'>{getOrders.products.qtd}</p>
+          <p className='products-price'>{getOrders.products.name}</p>
+        </div>
+        {/* <Button>
+          name='delivery'
+          className='btn-order'
+          type='submit'
+          onClick={}
+        </Button> */}
+        </div>
 
-        </section>
+      </section>
 
-        </main>
-      <Footer />
-    </Fragment>
-  )
+      </main>
+    <Footer />
+  </Fragment>
+)
 };
