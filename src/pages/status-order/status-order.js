6dfe90b46704e489a0f380be296dc0b7241330ca
caroline costa <carolmarcionilia@gmail.com>
2021-03-01 23:57:59 -0300

    testing order status components

--- src/pages/status-order/status-order.js
+++ src/pages/status-order/status-order.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import React from 'react'
 import Navbar from '../../components/generic-components/navbar/navbar'
+import AddStatusOrder from '../../components/generic-components/add-status-order/add-status-order'
 import './status-order.css'
 
 export const StatusOrder = () => {
@@ -9,6 +10,38 @@ export const StatusOrder = () => {
       <header>
         <Navbar />
       </header>
+      <main>
+        <AddStatusOrder
+          statusTitle='Pronto'
+          nextStatus='Entregar'
+          onClickNextStatus=''
+          orders={
+            (
+              [
+                {
+                  "id": 0,
+                  "client_name": "Carol",
+                  "user_id": 0,
+                  "table": 0,
+                  "status": "Pronto",
+                  "processedAt": "2021-03-02",
+                  "createdAt": "2021-03-02",
+                  "updatedAt": "2021-03-02",
+                  "Products": [
+                    {
+                      "id": 0,
+                      "name": "Hambúrguer simples",
+                      "flavor": "string",
+                      "complement": "string",
+                      "qtd": 2
+                    }
+                  ]
+                }
+              ]
+            )
+          }
+        />
+      </main>
     </Fragment >
   )
 }
