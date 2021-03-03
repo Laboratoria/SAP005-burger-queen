dc0a29e9d75fec22d6b7a42aa3aaccca32e37542
Carol Shimada <caroline_shimada@hotmail.com>
2021-03-03 18:17:18 -0300

    Primeira tentativa printar orders tela

--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -27,9 +27,9 @@ function Kitchen (){
       setList(dados) 
     })
   }, [tokenLocal])
-  const listDrinks = () => {
-    const productsDrinks = list;
-    setListMap(productsDrinks);
+  const listAllOrders = () => {
+    const productsList = list;
+    setListMap(productsList);
   } 
 
   return (
@@ -37,7 +37,7 @@ function Kitchen (){
         <Grid id='menuList'className='container' container direction="row" justify="flex-start" alignItems="flex-start">
           <NavBar/>   
         <h1>Kitchen</h1>
-        <button onClick={listDrinks} className={classes.submitMenuType} >Pedidos</button>
+        <button onClick={listAllOrders} className={classes.submitMenuType} >Pedidos</button>
         {listMap.map((product) => (
             <div key={product.id} name={product.client_name} table={product.table}>
               <button className={classes.submitMenuItems} >{product.client_name} {product.table} {product.Products.name}</button>
