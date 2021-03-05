3cada36ca45e7eac5cd55e3ae4477b7f2a014e4b
laboratoria <jucterin@gmail.com>
2021-03-05 12:41:28 -0300

    atualizaçao

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login.js";
+import Register from "./pages/Register/Register.js";
 import Hall from "./pages/Hall/Hall.js";
 import Kitchen from "./pages/Kitchen/Kitchen.js"
-import OrderReady from "./pages/OrderReady/OrderReady";
+import OrderReady from "./pages/OrderReady/OrderReady.js";
+import OrderFinished from "./pages/OrdersFinished/OrdersFinished.js"
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
@@ -19,6 +20,7 @@ ReactDOM.render(
       <Route path ="/Hall" component={Hall} exact/>
       <Route path ="/Kitchen" component={Kitchen} exact/>
       <Route path ="/OrderReady" component={OrderReady} exact/>
+      <Route path ="/OrderFinished" component={OrderFinished} exact/>
 
     </Switch>
   </BrowserRouter>,
