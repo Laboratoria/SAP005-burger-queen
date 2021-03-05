43d056a067740cb24469eae0585c7b47c72aad7a
KarinaFS <karina_fsantos@outlook.com>
2021-03-05 15:39:39 -0300

    css da cozinha pronto

--- src/pages/Kitchen/Kitchen.js
+++ src/pages/Kitchen/Kitchen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardText, CardGroup, Row, Col } from 'reactstrap';
+import { Card, CardTitle, CardText, CardGroup, Row, Col, Button } from 'reactstrap';
 import { useHistory } from 'react-router-dom';
 import './Kitchen.css';
 
@@ -79,10 +79,10 @@ export const Kitchen = () => {
   return (
     <div>
       <>
-      <svg onClick={handleSignOut}xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16" className="signout-btn-kitchen">
-        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
-        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-      </svg>
+        <svg onClick={handleSignOut} xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16" className="signout-btn-kitchen">
+          <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+          <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+        </svg>
         <h1 className="h1-kitchen">Pedidos Pendentes</h1>
         {pendingOrders.map((order) => {
           const createdDate = new Date(order.createdAt).toLocaleString()
@@ -91,13 +91,13 @@ export const Kitchen = () => {
               <Col>
                 <CardGroup>
                   <Card body className="text-center" key={order.id}>
+                    <CardTitle className="table-ready" tag="h5">Mesa: {order.table}</CardTitle>
                     <CardText>
                       <table className="table">
                         <tbody>
                           <tr>
                             <th>Pedido nº {order.id}</th>
                             <th>Cliente: {order.client_name}</th>
-                            <th>Mesa: {order.table}</th>
                             <th>Status: {order.status.replace("pending", "Pendente")}</th>
                             <th>Criado: {createdDate}</th>
                           </tr>
@@ -115,18 +115,13 @@ export const Kitchen = () => {
                               <td>{items.complement}</td>
                             </tr>
                           ))}
-                          <tr>
-                            <th>
-                              <button
-                                className="prepare-btn"
-                                onClick={() => handlePrepare(order)}>
-                                Preparar Pedido
-                              </button>
-                            </th>
-                          </tr>
                         </tbody>
                       </table>
                     </CardText>
+                    <Button
+                      onClick={() => handlePrepare(order)}>
+                      Preparar Pedido
+                    </Button>
                   </Card>
                 </CardGroup>
               </Col>
@@ -143,13 +138,13 @@ export const Kitchen = () => {
               <Col>
                 <CardGroup>
                   <Card body className="text-center" key={order.id}>
+                    <CardTitle className="table-ready" tag="h5">Mesa: {order.table}</CardTitle>
                     <CardText>
                       <table className="table">
                         <tbody>
                           <tr>
                             <th>Pedido nº {order.id}</th>
                             <th>Cliente: {order.client_name}</th>
-                            <th>Mesa: {order.table}</th>
                             <th>Status: {order.status.replace("preparing", "Preparando")}</th>
                             <th>Criado: {createdDateString}</th>
                             <th>Atualizado: {updatedDate}</th>
@@ -168,18 +163,13 @@ export const Kitchen = () => {
                               <td>{items.complement}</td>
                             </tr>
                           ))}
-                          <tr>
-                            <th>
-                              <button
-                                className="finish-btn"
-                                onClick={() => handleFinish(order)}>
-                                Pedido Pronto
-                              </button>
-                            </th>
-                          </tr>
                         </tbody>
                       </table>
                     </CardText>
+                    <Button
+                      onClick={() => handleFinish(order)}>
+                      Pedido Pronto
+                    </Button>
                   </Card>
                 </CardGroup>
               </Col>
